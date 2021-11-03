ff4bf44bd648ff179bd7c82d0cf80da968b291b0
David Ichim <ichim.david@gmail.com>
2021-11-03 12:20:00 +0200

    Refs #141204 added customizations folder from volto-ims-theme to volto-eea-design-system:
    - this way the design team can work on the changes needed for the header and footer plus
      whatever else we have customized.

---
+++ src/customizations/components/theme/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,125 @@
+/**
+ * Breadcrumbs components.
+ * @module components/theme/Breadcrumbs/Breadcrumbs
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { compose } from 'redux';
+import { Link } from 'react-router-dom';
+import { Breadcrumb, Container, Segment, Icon } from 'semantic-ui-react';
+import { defineMessages, injectIntl } from 'react-intl';
+
+import { getBreadcrumbs } from '@plone/volto/actions';
+import { getBaseUrl } from '@plone/volto/helpers';
+
+const messages = defineMessages({
+  home: {
+    id: 'Home',
+    defaultMessage: 'Home',
+  },
+  breadcrumbs: {
+    id: 'Breadcrumbs',
+    defaultMessage: 'Breadcrumbs',
+  },
+});
+
+/**
+ * Breadcrumbs container class.
+ * @class Breadcrumbs
+ * @extends Component
+ */
+class Breadcrumbs extends Component {
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    getBreadcrumbs: PropTypes.func.isRequired,
+    pathname: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ).isRequired,
+  };
+
+  /**
+   * Component will mount
+   * @method componentWillMount
+   * @returns {undefined}
+   */
+  UNSAFE_componentWillMount() {
+    this.props.getBreadcrumbs(getBaseUrl(this.props.pathname));
+  }
+
+  /**
+   * Component will receive props
+   * @method componentWillReceiveProps
+   * @param {Object} nextProps Next properties
+   * @returns {undefined}
+   */
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.pathname !== this.props.pathname) {
+      this.props.getBreadcrumbs(getBaseUrl(nextProps.pathname));
+    }
+  }
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    return (
+      <Segment
+        role="navigation"
+        aria-label={this.props.intl.formatMessage(messages.breadcrumbs)}
+        className="breadcrumbs"
+        secondary
+        vertical
+      >
+        <Container>
+          <Breadcrumb>
+            <Link
+              to="/"
+              className="section"
+              title={this.props.intl.formatMessage(messages.home)}
+            >
+              <Icon name="home" />
+            </Link>
+            {this.props.items.map((item, index, items) => [
+              <Breadcrumb.Divider
+                icon="angle right"
+                key={`divider-${item.url}`}
+              />,
+              index < items.length - 1 ? (
+                <Link key={item.url} to={item.url} className="section">
+                  {item.title}
+                </Link>
+              ) : (
+                <Breadcrumb.Section key={item.url} active>
+                  {item.title}
+                </Breadcrumb.Section>
+              ),
+            ])}
+          </Breadcrumb>
+        </Container>
+      </Segment>
+    );
+  }
+}
+
+export default compose(
+  injectIntl,
+  connect(
+    (state) => ({
+      items: state.breadcrumbs.items,
+    }),
+    { getBreadcrumbs },
+  ),
+)(Breadcrumbs);
