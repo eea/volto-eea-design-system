0e16fdfc77e119da4e1f15183d9cb95ef9b4b6cf
David Ichim <ichim.david@gmail.com>
2021-11-24 10:50:50 +0200

    Refs #140454 changed link to the start of the docs

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/eea-design-system/intro"
+            to="/docs/intro"
           >
             Start with the EEA Design System
           </Link>
