80d38a96284e7fa9416ba327a43e29eb94bebe55
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-12-05 13:24:46 +0200

    Load footer in segment

--- src/DemoStory.stories.js
+++ src/DemoStory.stories.js
@@ -1,12 +1,15 @@
 import { injectIntl } from 'react-intl';
 import React from 'react';
 import { Footer } from '@plone/volto/components';
+import { Segment } from 'semantic-ui-react';
 import Wrapper from '@plone/volto/storybook';
 
 export const Default = injectIntl(({ children, ...args }) => {
   return (
     <Wrapper location={{ pathname: '/folder2/folder21/doc212' }}>
-      <Footer />
+      <Segment className="portal-footer">
+        <Footer />
+      </Segment>
     </Wrapper>
   );
 });
