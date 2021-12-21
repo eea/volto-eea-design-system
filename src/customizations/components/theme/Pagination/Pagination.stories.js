b1af226b9bd42097dc3c74be3d9ca3fab29b80f6
ITML <stamatis@itml.gr>
2021-12-21 14:25:44 +0000

    Pagination component stories minor changes

--- src/customizations/components/theme/Pagination/Pagination.stories.js
+++ src/customizations/components/theme/Pagination/Pagination.stories.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import { Pagination } from 'semantic-ui-react'
+import React from 'react';
+import { Pagination } from 'semantic-ui-react';
 
 export default {
-    title:'Components/Pagination',
-    component: Pagination,
-    parameters: {
-        actions: {
-          handles: ['click'],
-        },
-      },
-    argTypes:{}
-}
+  title: 'Components/Pagination',
+  component: Pagination,
+  parameters: {
+    actions: {
+      handles: ['click'],
+    },
+  },
+  argTypes: {},
+};
 
 const Template = (args) => <Pagination {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    defaultActivePage: 5,
-    totalPages: 10,
-}
+  defaultActivePage: 5,
+  totalPages: 10,
+};
 
 export const Playground = Template.bind({});
 Playground.args = {
-    defaultActivePage: 5,
-    totalPages: 10,
-    disabled:false,
-    pointing:false,
-    secondary:false,
-    boundaryRange:0,
-    siblingRange:1,
-}
+  defaultActivePage: 5,
+  totalPages: 10,
+  disabled: false,
+  pointing: false,
+  secondary: false,
+  boundaryRange: 0,
+  siblingRange: 1,
+};
