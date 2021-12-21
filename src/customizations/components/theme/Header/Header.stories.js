b9f6d05708310889dd018961bf5df9d5edd66e12
ITML <stamatis@itml.gr>
2021-12-21 14:00:31 +0000

    renamed Demo folder to Layout

--- src/customizations/components/theme/Header/Header.stories.js
+++ src/customizations/components/theme/Header/Header.stories.js
@@ -6,7 +6,7 @@ import { Segment } from 'semantic-ui-react';
 import Wrapper from '@plone/volto/storybook';
 
 export default {
-  title: 'Demo/Header',
+  title: 'Layout/Header',
   cocmponent: Header,
   argTypes: {},
   decorators: [
