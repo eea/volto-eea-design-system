a87cb6ad7959ce939c9decbfaa08026d4f00a10e
ITML <stamatis@itml.gr>
2021-12-21 14:06:30 +0000

    Comment component stories minor changes

--- src/customizations/components/theme/Comment/Comment.stories.js
+++ src/customizations/components/theme/Comment/Comment.stories.js
@@ -1,29 +1,54 @@
-import React from 'react'
-import CommentEEA from './Comment'
-import { Icon } from 'semantic-ui-react'
+import React from 'react';
+import CommentEEA from './Comment';
+import { Icon } from 'semantic-ui-react';
 
 export default {
-    title: 'Components/Comment',
-    component: CommentEEA,
-    argTypes: {
-        size:{
-            control:{type:'select'},
-            options:['mini','tine','small','large','big','huge','massive']
-        }
-    }
-}
+  title: 'Components/Comment',
+  component: CommentEEA,
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['mini', 'tine', 'small', 'large', 'big', 'huge', 'massive'],
+    },
+  },
+};
 
-const Template = (args) => <CommentEEA {...args}></CommentEEA>
+const Template = (args) => <CommentEEA {...args}></CommentEEA>;
 
 export const Default = Template.bind({});
 Default.args = {
-    title:"Comments Header",
-    comments: [
-        {src:"",author:"User 1",time:"Message timestamp",text:"Message Content",replies:[{src:"",author:"User 4",time:"Message timestamp",text:"Message Content"}]},
-        {src:"",author:"User 2",time:"Message timestamp",text:"Message Content",replies:[]},
-        {src:"",author:"User 2",time:"Message timestamp",text:"Message Content",replies:[]},
-    ],
-    threaded:false,
-    minimal:false,
-    size:'large'
-}
+  title: 'Comments Header',
+  comments: [
+    {
+      src: '',
+      author: 'User 1',
+      time: 'Message timestamp',
+      text: 'Message Content',
+      replies: [
+        {
+          src: '',
+          author: 'User 4',
+          time: 'Message timestamp',
+          text: 'Message Content',
+        },
+      ],
+    },
+    {
+      src: '',
+      author: 'User 2',
+      time: 'Message timestamp',
+      text: 'Message Content',
+      replies: [],
+    },
+    {
+      src: '',
+      author: 'User 2',
+      time: 'Message timestamp',
+      text: 'Message Content',
+      replies: [],
+    },
+  ],
+  threaded: false,
+  minimal: false,
+  size: 'large',
+};
