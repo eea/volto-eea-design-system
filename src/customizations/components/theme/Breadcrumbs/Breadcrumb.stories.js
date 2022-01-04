3d0d17ca4f04123d55fda64c516b8f867ee1bd13
ITML <stamatis@itml.gr>
2022-01-04 09:24:46 +0000

    Breadcrumb stories minor changes

--- src/customizations/components/theme/Breadcrumbs/Breadcrumb.stories.js
+++ src/customizations/components/theme/Breadcrumbs/Breadcrumb.stories.js
@@ -21,15 +21,25 @@ export default {
   },
 };
 
-const Template = (args) => <Breadcrumb {...args}></Breadcrumb>;
+//const Template = (args) => <Breadcrumb {...args}></Breadcrumb>;
+
+const Template = (args) => 
+      <Breadcrumb size={args.size}>
+          {args.sections.map((section,index) => (
+            <>
+              <Breadcrumb.Section key={section.key} link={section.link} active={index === args.sections.length - 1}>{section.content} </Breadcrumb.Section>
+              {index !== args.sections.length - 1 && <Breadcrumb.Divider icon={`${args.icon}`} />}
+            </>
+          ))}
+      </Breadcrumb>;
 
 export const Default = Template.bind({});
 Default.args = {
   sections: [
     { key: 'Section 1', content: 'Section 1', link: true },
-    { key: 'Section ', content: 'Section 2', link: true },
+    { key: 'Section 2', content: 'Section 2', link: true },
     { key: 'Section 3', content: 'Section 3', link: true },
   ],
-  icon: 'right arrow',
+  icon: 'right chevron',
   size: 'small',
 };
