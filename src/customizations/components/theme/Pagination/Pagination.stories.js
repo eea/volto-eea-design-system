2a80175762fbab23691d52f89cd38eb6f7740c4e
ITML <stamatis@itml.gr>
2022-01-14 17:01:49 +0000

    Pagination theming

--- src/customizations/components/theme/Pagination/Pagination.stories.js
+++ src/customizations/components/theme/Pagination/Pagination.stories.js
@@ -12,21 +12,19 @@ export default {
   argTypes: {},
 };
 
-const Template = (args) => <Pagination {...args} />;
+const Template = (args) => <Pagination className="eeaPagination" {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   defaultActivePage: 5,
+//   totalPages: 10,
+// };
 
 export const Default = Template.bind({});
 Default.args = {
   defaultActivePage: 5,
   totalPages: 10,
-};
-
-export const Playground = Template.bind({});
-Playground.args = {
-  defaultActivePage: 5,
-  totalPages: 10,
   disabled: false,
-  pointing: false,
-  secondary: false,
   boundaryRange: 0,
   siblingRange: 1,
 };
