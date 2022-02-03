fee446668a8d65a717b42667af825cb5cec836b2
ITML <stamatis@itml.gr>
2022-02-03 12:13:59 +0000

    refactor(pullquote):adjust to volto theming

--- src/customizations/components/theme/Pullquote/Pullquote.js
+++ src/customizations/components/theme/Pullquote/Pullquote.js
@@ -3,13 +3,13 @@ import { Icon } from 'semantic-ui-react';
 
 function Pullquote({ quote, source, quotePosition }) {
   return (
-    <blockquote className={`pullquote eea-pullquote-${quotePosition}`}>
+    <blockquote className={`eea pullquote ${quotePosition}`}>
       <Icon name="quote left"></Icon>
-      <div className="eea-pullquote-body">
-        <h4 className="eea-pullquote-quote">{quote}</h4>
-        <p className="eea-pullquote-author">{source}</p>
+      <div className="content">
+        <h4 className="quote">{quote}</h4>
+        <p className="author">{source}</p>
       </div>
-      <Icon className="eea-quote-right" name="quote right"></Icon>
+      <Icon className="quote right" name="quote right"></Icon>
     </blockquote>
   );
 }
