3c945e1600b4beb3667b389542adade317d285c6
ITML <stamatis@itml.gr>
2022-02-04 09:11:54 +0000

    refactor(label): adjust to volto theming

--- src/customizations/components/theme/Label/Label.stories.js
+++ src/customizations/components/theme/Label/Label.stories.js
@@ -34,14 +34,14 @@ export default {
 
 const Template = (args) => <Label {...args}></Label>;
 
-export const LowImportance = Template.bind({});
-LowImportance.args = {
+export const Default = Template.bind({});
+Default.args = {
   content: 'Label',
   pointing: false,
 };
 
 export const MediumImportance = (args) => (
-  <Label className="medium-label" {...args}></Label>
+  <Label className="medium" {...args}></Label>
 );
 MediumImportance.args = {
   content: 'Label',
@@ -49,7 +49,7 @@ MediumImportance.args = {
 };
 
 export const HighImportance = (args) => (
-  <Label className="high-label" {...args}></Label>
+  <Label className="high" {...args}></Label>
 );
 HighImportance.args = {
   content: 'Label',
@@ -57,116 +57,9 @@ HighImportance.args = {
 };
 
 export const Highlight = (args) => (
-  <Label className="highlight-label" {...args}></Label>
+  <Label className="highlight" {...args}></Label>
 );
 Highlight.args = {
   content: 'Label',
   pointing: false,
 };
-// export const Default = Template.bind({});
-// Default.args = {
-//   children: ['Label'],
-// };
-
-// export const Playground = Template.bind({});
-// Playground.args = {
-//   children: ['Label'],
-//   color: 'blue',
-//   pointing: true,
-//   tag: false,
-//   ribbon: false,
-//   floating: false,
-//   attached: 'top left',
-// };
-
-// Playground.argTypes = {
-//   color: {
-//     description: 'color of the label',
-//     table: {
-//       type: {
-//         summary: 'string',
-//       },
-//       defaultValue: {
-//         summary: '',
-//       },
-//     },
-//   },
-//   tag: {
-//     description: 'a label can appear as a tag',
-//     table: {
-//       type: {
-//         summary: 'boolean',
-//       },
-//       defaultValue: {
-//         summary: false,
-//       },
-//     },
-//   },
-//   ribbon: {
-//     description:
-//       'a label can appear as a ribbon attaching itself to an element',
-//     table: {
-//       type: {
-//         summary: 'boolean | string',
-//       },
-//       defaultValue: {
-//         summary: '',
-//       },
-//     },
-//   },
-//   floating: {
-//     description: 'float above another element in the upper right corner',
-//     table: {
-//       type: {
-//         summary: 'boolean',
-//       },
-//       defaultValue: {
-//         summary: false,
-//       },
-//     },
-//   },
-// };
-
-// export const Link = Template.bind({});
-// Link.args = {
-//   children: ['Label'],
-//   as: 'a',
-//   href: 'https://react.semantic-ui.com/elements/label/#types-basic',
-//   color: 'blue',
-// };
-
-// Link.argTypes = {
-//   as: {
-//     description: 'an element type to render as (string or function)',
-//     table: {
-//       type: {
-//         summary: 'elementType',
-//       },
-//       defaultValue: {
-//         summary: '',
-//       },
-//     },
-//   },
-//   href: {
-//     description: 'link url',
-//     table: {
-//       type: {
-//         summary: 'string',
-//       },
-//       defaultValue: {
-//         summary: '',
-//       },
-//     },
-//   },
-//   color: {
-//     description: 'color of the label',
-//     table: {
-//       type: {
-//         summary: 'string',
-//       },
-//       defaultValue: {
-//         summary: '',
-//       },
-//     },
-//   },
-// };
