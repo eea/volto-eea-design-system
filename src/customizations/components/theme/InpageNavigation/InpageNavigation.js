97ef50069cd9b29345fb68ce8b557a47577669a4
ITML <stamatis@itml.gr>
2022-02-08 16:51:59 +0000

    fix(inpageNavigation): hide at the top of the page

--- src/customizations/components/theme/InpageNavigation/InpageNavigation.js
+++ src/customizations/components/theme/InpageNavigation/InpageNavigation.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Icon } from 'semantic-ui-react';
 
 function InpageNavigation({ linkId }) {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [removeClass, setRemoveClass] = useState('hidden');
+
+  const handleInpageNavigationVisibility = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+
+    if (scrollPosition > 50) {
+      return setRemoveClass('');
+    } else {
+      return setRemoveClass('hidden');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleInpageNavigationVisibility);
+  });
+
+  const onInpageNavigationClick = () => {
+    //setRemoveClass("hidden",() =>{window.scrollTo(0,0);});
+    window.scrollTo(0, 0);
+  };
+
   return (
     <Container>
       <div
         id="inpage-navigation"
-        onClick={() => window.scrollTo(0, 0)}
+        onClick={onInpageNavigationClick}
         role="none"
+        className={removeClass}
       >
         <div id="navigation-text-tablet">
           <Icon name="chevron up" />
