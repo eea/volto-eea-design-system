1d7afdc8d751e61c3eef75360555807fdbef8619
ITML <a.sourtzis@itml.gr>
2022-02-10 10:56:15 +0000

    refactor(InpageNavigation): clear up code

--- src/customizations/components/theme/InpageNavigation/InpageNavigation.js
+++ src/customizations/components/theme/InpageNavigation/InpageNavigation.js
@@ -1,45 +1,39 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { Component } from 'react';
 import { Container, Icon } from 'semantic-ui-react';
 
 class InpageNavigation extends Component {
-  // const [scrollPosition, setScrollPosition] = useState(0);
-  // const [removeClass, setRemoveClass] = useState('hidden');
-
   constructor(props) {
     super(props);
     this.state = {
       scrollPosition: 0,
-      removeClass: 'hidden'
+      removeClass: 'hidden',
     };
   }
 
   handleInpageNavigationVisibility = () => {
     const position = window.scrollY;
-    this.setState({scrollPosition: position})
-    //setScrollPosition(position);
+    this.setState({ scrollPosition: position });
 
     if (this.state.scrollPosition > 50) {
-      return this.setState({removeClass: ''});
+      return this.setState({ removeClass: '' });
     } else {
-      return this.setState({removeClass: 'hidden'});
+      return this.setState({ removeClass: 'hidden' });
     }
   };
 
   componentDidMount() {
-    window.addEventListener("scroll",this.handleInpageNavigationVisibility);
+    window.addEventListener('scroll', this.handleInpageNavigationVisibility);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("scroll",this.handleInpageNavigationVisibility);
+    window.removeEventListener('scroll', this.handleInpageNavigationVisibility);
   }
 
   onInpageNavigationClick = () => {
-    //setRemoveClass("hidden",() =>{window.scrollTo(0,0);});
     window.scrollTo(0, 0);
   };
 
   render() {
-
     return (
       <Container>
         <div
