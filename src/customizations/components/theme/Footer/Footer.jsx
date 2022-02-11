c3d14871887a83fb91792fbc8977c0439bcb16f2
ITML <a.sourtzis@itml.gr>
2022-02-11 08:45:37 +0000

    refactor(Footer): add sub components and props

--- src/customizations/components/theme/Footer/Footer.jsx
+++ src/customizations/components/theme/Footer/Footer.jsx
@@ -14,17 +14,6 @@ import { Link } from 'react-router-dom';
 import EIONETLogo from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/EIONETLogo.png';
 import EEALogo from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/EEA-Logo-white.svg';
 
-import Logo1 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group.svg';
-import Logo2 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-3.svg';
-import Logo3 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-154.svg';
-import Logo4 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-95.svg';
-import Logo5 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-2.svg';
-import Logo6 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-98.svg';
-import Logo7 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-5.svg';
-import Logo8 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-96.svg';
-import Logo9 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-4.svg';
-import Logo10 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-1.svg';
-
 /**
  * Component to display the footer.
  * @function Footer
@@ -34,54 +23,17 @@ import Logo10 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/i
 
 class Footer extends Component {
   render() {
-    const actions = [
-      { link: '/#', title: 'CMS Login', copy: false },
-      { link: '/#', title: 'Sitemap', copy: false },
-      { link: '/#', title: 'Privacy', copy: false },
-      { link: '/#', title: 'Copyright', copy: true },
-    ];
-
-    const sites = [
-      { link: '/#', src: Logo1, alt: 'Biodiversity' },
-      { link: '/#', src: Logo2, alt: 'Climate and energy in the EU' },
-      { link: '/#', src: Logo3, alt: 'Copernicus in situ' },
-      {
-        link: '/#',
-        src: Logo4,
-        alt: 'Information platform for chemical monitoring',
-      },
-      { link: '/#', src: Logo5, alt: 'WISE freshwater' },
-      { link: '/#', src: Logo6, alt: 'Forest information system for europe' },
-      { link: '/#', src: Logo7, alt: 'Climate adapt' },
-      { link: '/#', src: Logo8, alt: 'Copernicus land monitoring service' },
-      { link: '/#', src: Logo9, alt: 'European industrial emissions portal' },
-      { link: '/#', src: Logo10, alt: 'WISE marine' },
-    ];
-
-    const social = [
-      { name: 'twitter', link: '/#' },
-      { name: 'facebook', link: '/#' },
-      { name: 'linkedin', link: '/#' },
-      { name: 'youtube', link: '/#' },
-      { name: 'rss', link: '/#' },
-    ];
-
-    const contacts = {
-      header: 'Contact Us',
-      contacts: [
-        { icon: 'comment outline', text: 'Ask your question' },
-        { icon: 'envelope outline', text: 'Sign up to our newsletter' },
-      ],
-      address: 'Kongens Nytorv 6 1050 Copenhagen K (+45) 33 36 71 00',
-    };
-
     return (
       <footer>
         <div className="visual">
           <Container>
             <div className="theme-sites">
-              <Footer.Header>The EEA also contributes to</Footer.Header>
-              <Footer.Sites sites={sites}></Footer.Sites>
+              {this.props.header && (
+                <Footer.Header>{this.props.header}</Footer.Header>
+              )}
+              {this.props.sites.length > 0 && (
+                <Footer.Sites sites={this.props.sites}></Footer.Sites>
+              )}
             </div>
             <div className="subfooter">
               <Grid>
@@ -108,14 +60,22 @@ class Footer extends Component {
                 </Grid.Column>
                 <Grid.Column mobile={6} tablet={5} computer={4}>
                   <div className="item">
-                    <Footer.Contact contacts={contacts}></Footer.Contact>
+                    {this.props.contacts && (
+                      <Footer.Contact
+                        contacts={this.props.contacts}
+                      ></Footer.Contact>
+                    )}
 
-                    <Footer.Social social={social}></Footer.Social>
+                    {this.props.social.length > 0 && (
+                      <Footer.Social social={this.props.social}></Footer.Social>
+                    )}
                   </div>
                 </Grid.Column>
               </Grid>
               <Grid.Row>
-                <Footer.Actions actions={actions} />
+                {this.props.actions.length > 0 && (
+                  <Footer.Actions actions={this.props.actions} />
+                )}
               </Grid.Row>
             </div>
           </Container>
@@ -128,8 +88,8 @@ class Footer extends Component {
 const Actions = (props) => (
   <div className="menu">
     {props.actions &&
-      props.actions.map((action) => (
-        <a href={action.link}>
+      props.actions.map((action, index) => (
+        <a href={action.link} key={index}>
           {action.copy && <>&copy;</>}
           {action.title}
         </a>
@@ -143,8 +103,8 @@ const Sites = (props) => (
   <div className="logos">
     <Grid columns={5}>
       {props.sites &&
-        props.sites.map((site) => (
-          <Grid.Column className="logo">
+        props.sites.map((site, index) => (
+          <Grid.Column className="logo" key={index}>
             <Link to={site.link}>
               <Image src={site.src} alt={site.alt}></Image>
             </Link>
@@ -162,8 +122,8 @@ Footer.Header = Header;
 
 const Social = (props) => (
   <div className="social">
-    {props.social.map((item) => (
-      <a href={item.link} aria-label={`${item.name} link`}>
+    {props.social.map((item, index) => (
+      <a href={item.link} aria-label={`${item.name} link`} key={index}>
         <Icon name={item.name}></Icon>
       </a>
     ))}
@@ -176,8 +136,8 @@ const Contact = (props) => (
   <>
     <p className="header">{props.contacts.header}</p>
     {props.contacts.contacts.length > 0 &&
-      props.contacts.contacts.map((contact) => (
-        <div className="contact">
+      props.contacts.contacts.map((contact, index) => (
+        <div className="contact" key={index}>
           <Icon name={contact.icon} size="big"></Icon>
           {contact.text}
         </div>
