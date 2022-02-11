57367dd2f8f672dda1966ada644939e5496daac0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-02-11 15:21:44 +0200

    Change name of args

--- src/ui/Blockquote/Blockquote.stories.jsx
+++ src/ui/Blockquote/Blockquote.stories.jsx
@@ -11,77 +11,72 @@ export default {
   title: 'Components/Blockquote (Edw model)',
   component: Blockquote,
   argTypes: {
-    citation: {
-      title: 'Content',
-      description: 'Blockquote citation',
+    content: {
+      name: 'Content',
+      description: 'Blockquote content',
       type: { name: 'string', required: true },
-      // table: {
-      //   defaultValue: { summary: '""' },
-      // },
     },
-    author: {
-      description: 'Author of the citation',
+    meta: {
+      name: 'Meta',
+      description: 'Author of the content',
       type: { name: 'string', required: true },
-      // table: {
-      //   defaultValue: { summary: '""' },
-      // },
     },
   },
 };
 
 export const Default = (args) => (
   <Blockquote>
-    <Blockquote.Content>{args.citation}</Blockquote.Content>
-    <Blockquote.Meta>{args.author}</Blockquote.Meta>
+    <Blockquote.Content>{args.content}</Blockquote.Content>
+    <Blockquote.Meta>{args.meta}</Blockquote.Meta>
   </Blockquote>
 );
 
 Default.args = {
-  citation: CITATION,
-  author: AUTHOR,
+  content: CITATION,
+  meta: AUTHOR,
 };
 
 export const Reversed = (args) => (
   <Blockquote>
-    <Blockquote.Meta>{args.author}</Blockquote.Meta>
-    <Blockquote.Content>{args.citation}</Blockquote.Content>
+    <Blockquote.Meta>{args.meta}</Blockquote.Meta>
+    <Blockquote.Content>{args.content}</Blockquote.Content>
   </Blockquote>
 );
 
 Reversed.args = {
-  citation:
+  content:
     'The Ministry of Environment & Energy is responsible for environmental policy, the preparation of plans and programmes, and overseeing their implementation. The Ministry is also in charge of the transposition of EU environmental Directives into national law.',
-  author: '22 December 1989',
+  meta: '22 December 1989',
 };
 
 export const BlockquoteWithImageAside = (args) => (
   <Blockquote>
     <Blockquote.Content as="div">
-      <Image src={args.src} size="tiny" floated="right" />
-      {args.citation}
+      <Image src={args.imgSrc} size="tiny" floated="right" />
+      {args.content}
     </Blockquote.Content>
-    <Blockquote.Meta>{args.author}</Blockquote.Meta>
+    <Blockquote.Meta>{args.meta}</Blockquote.Meta>
   </Blockquote>
 );
 
 BlockquoteWithImageAside.args = {
-  citation: CITATION,
-  author: AUTHOR,
-  src: IMG,
+  content: CITATION,
+  meta: AUTHOR,
+  imgSrc: IMG,
 };
 
 export const BlockquoteWithImage = (args) => (
   <Blockquote>
     <Blockquote.Content>
-      <Image src={args.src} size="tiny" />
-      {args.citation}
+      <Image src={args.imgSrc} size="tiny" />
+      {args.content}
     </Blockquote.Content>
-    <Blockquote.Meta>{args.author}</Blockquote.Meta>
+    <Blockquote.Meta>{args.meta}</Blockquote.Meta>
   </Blockquote>
 );
 
 BlockquoteWithImage.args = {
-  citation: CITATION,
-  author: AUTHOR,
-  src: IMG,
+  content: CITATION,
+  meta: AUTHOR,
+  imgSrc: IMG,
 };
