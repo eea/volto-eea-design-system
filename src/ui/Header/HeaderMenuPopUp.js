ff013afc68fed97d085e3930f6d728b266786dc1
ITML <antonis@itml.gr>
2022-02-21 16:54:09 +0000

    refactor(megaMenu): new implementation

--- src/ui/Header/HeaderMenuPopUp.js
+++ src/ui/Header/HeaderMenuPopUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 
 function HeaderMenuPopUp() {
   const menuItems = [
@@ -142,59 +143,61 @@ function HeaderMenuPopUp() {
   ];
 
   return (
-    <div id="eea-mega-menu-wrap">
-      <nav>
-        <ul className="menu">
-          {menuItems.map((item) => (
-            //first tier
-            <li key={item['@id']}>
-              {item.items.length > 0 && (
-                <label htmlFor={`drop-${item['@id']}`} className="toggle">
-                  {item.title}
-                </label>
-              )}
-              <a href="/#">{item.title}</a>
-              {item.items.length > 0 && (
-                <input type="checkbox" id={`drop-${item['@id']}`} />
-              )}
-              {item.items.length > 0 && (
-                //second tier
-                <ul className="secondmenu">
-                  {item.items.map((section) => (
-                    <li key={section['@id']}>
-                      {section.items.length > 0 && (
-                        <label
-                          htmlFor={`drop-${item['@id']}-${section['@id']}`}
-                          className="toggle"
-                        >
-                          {section.title}
-                        </label>
-                      )}
-                      <a href="/#">{section.title}</a>
-                      {section.items.length > 0 && (
-                        <input
-                          type="checkbox"
-                          id={`drop-${item['@id']}-${section['@id']}`}
-                        ></input>
-                      )}
-                      {section.items.length > 0 && (
-                        //third tier
-                        <ul className="thirdmenu">
-                          {section.items.map((page) => (
-                            <li key={page['@id']}>
-                              <a href="/#">{page.title}</a>
-                            </li>
-                          ))}
-                        </ul>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <div id="mega-menu">
+      <Container>
+        <nav>
+          <ul className="menu">
+            {menuItems.map((item) => (
+              //first tier
+              <li key={item['@id']}>
+                {item.items.length > 0 && (
+                  <label htmlFor={`drop-${item['@id']}`} className="toggle">
+                    {item.title}
+                  </label>
+                )}
+                <a href="/#">{item.title}</a>
+                {item.items.length > 0 && (
+                  <input type="checkbox" id={`drop-${item['@id']}`} />
+                )}
+                {item.items.length > 0 && (
+                  //second tier
+                  <ul className="sub second">
+                    {item.items.map((section) => (
+                      <li key={section['@id']}>
+                        {section.items.length > 0 && (
+                          <label
+                            htmlFor={`drop-${item['@id']}-${section['@id']}`}
+                            className="toggle"
+                          >
+                            {section.title}
+                          </label>
+                        )}
+                        <a href="/#">{section.title}</a>
+                        {section.items.length > 0 && (
+                          <input
+                            type="checkbox"
+                            id={`drop-${item['@id']}-${section['@id']}`}
+                          ></input>
+                        )}
+                        {section.items.length > 0 && (
+                          //third tier
+                          <ul className="sub third">
+                            {section.items.map((page) => (
+                              <li key={page['@id']}>
+                                <a href="/#">{page.title}</a>
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </Container>
     </div>
   );
 }
