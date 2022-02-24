faafcffe07383ba55f5746cb971cdf706d988f78
nileshgulia1 <nileshgulia@gmail.com>
2022-02-24 20:31:36 +0530

    refactor Footer component to consume children and prop

---
+++ src/ui/Footer/Contact.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+const Contact = (props) => {
+  if (props.children) {
+    return <div>{props.children}</div>;
+  }
+  return (
+    <>
+      <p className="header">{props.contacts.header}</p>
+      {props.contacts.contacts.length > 0 &&
+        props.contacts.contacts.map((contact, index) => (
+          <div className="contact" key={index}>
+            <Icon name={contact.icon} size="big"></Icon>
+            {contact.text}
+          </div>
+        ))}
+      {props.contacts.address && (
+        <p className="address">{props.contacts.address}</p>
+      )}
+    </>
+  );
+};
+
+Contact.propTypes = {
+  contacts: PropTypes.shape({
+    header: PropTypes.string,
+    contacts: PropTypes.array,
+  }),
+};
+
+export default Contact;
