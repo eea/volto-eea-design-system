4e5742872f21d9737f3de443fa0e9d1b6ba02637
nileshgulia1 <nileshgulia@gmail.com>
2022-02-28 14:58:04 +0530

    update footer stories

--- src/ui/Footer/Footer.stories.js
+++ src/ui/Footer/Footer.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
-
+import { Grid, Image } from 'semantic-ui-react';
 import Logo1 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group.svg';
 import Logo2 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-3.svg';
 import Logo3 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-154.svg';
@@ -11,6 +11,8 @@ import Logo7 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/im
 import Logo8 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-96.svg';
 import Logo9 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-4.svg';
 import Logo10 from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/Extras/Group-1.svg';
+import EIONETLogo from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/EIONETLogo.png';
+import EEALogo from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Footer/EEA-Logo-white.svg';
 
 export default {
   title: 'Layout/Footer',
@@ -18,46 +20,85 @@ export default {
   argTypes: {},
 };
 
-const Template = (args) => <Footer {...args}></Footer>;
+const Template = (actions, sites, social, contacts) => (
+  <Footer>
+    <Footer.Header>The EEA also contributes to</Footer.Header>
+    <Footer.Sites sites={sites} />
+    <Footer.SubFooter>
+      <div className="subfooter">
+        <Grid>
+          <Grid.Column mobile={6} tablet={7} computer={8}>
+            <div className="item">
+              <div className="site logo">
+                <Image src={EEALogo} alt="EEA Logo"></Image>
+                <p className="description">An agency of the European Union</p>
+              </div>
+
+              <div className="eionet logo">
+                <Image src={EIONETLogo} alt="EIONET Logo"></Image>
+                <p className="description">
+                  European Environment
+                  <br />
+                  Information and
+                  <br />
+                  Observation Network
+                </p>
+              </div>
+            </div>
+          </Grid.Column>
+          <Grid.Column mobile={6} tablet={5} computer={4}>
+            <div className="item">
+              <Footer.Contact contacts={contacts} />
+
+              <Footer.Social social={social} />
+            </div>
+          </Grid.Column>
+        </Grid>
+        <Grid.Row>
+          <Footer.Actions actions={actions} />
+        </Grid.Row>
+      </div>
+    </Footer.SubFooter>
+  </Footer>
+);
 
 export const Default = Template.bind({});
-Default.args = {
-  header: 'The EEA also contributes to',
-  actions: [
-    { link: '/#', title: 'CMS Login', copy: false },
-    { link: '/#', title: 'Sitemap', copy: false },
-    { link: '/#', title: 'Privacy', copy: false },
-    { link: '/#', title: 'Copyright', copy: true },
-  ],
-  sites: [
-    { link: '/#', src: Logo1, alt: 'Biodiversity' },
-    { link: '/#', src: Logo2, alt: 'Climate and energy in the EU' },
-    { link: '/#', src: Logo3, alt: 'Copernicus in situ' },
-    {
-      link: '/#',
-      src: Logo4,
-      alt: 'Information platform for chemical monitoring',
-    },
-    { link: '/#', src: Logo5, alt: 'WISE freshwater' },
-    { link: '/#', src: Logo6, alt: 'Forest information system for europe' },
-    { link: '/#', src: Logo7, alt: 'Climate adapt' },
-    { link: '/#', src: Logo8, alt: 'Copernicus land monitoring service' },
-    { link: '/#', src: Logo9, alt: 'European industrial emissions portal' },
-    { link: '/#', src: Logo10, alt: 'WISE marine' },
-  ],
-  social: [
-    { name: 'twitter', link: '/#' },
-    { name: 'facebook', link: '/#' },
-    { name: 'linkedin', link: '/#' },
-    { name: 'youtube', link: '/#' },
-    { name: 'rss', link: '/#' },
-  ],
-  contacts: {
-    header: 'Contact Us',
-    contacts: [
-      { icon: 'comment outline', text: 'Ask your question' },
-      { icon: 'envelope outline', text: 'Sign up to our newsletter' },
-    ],
-    address: 'Kongens Nytorv 6 1050 Copenhagen K (+45) 33 36 71 00',
+Default.actions = [
+  { link: '/#', title: 'CMS Login', copy: false },
+  { link: '/#', title: 'Sitemap', copy: false },
+  { link: '/#', title: 'Privacy', copy: false },
+  { link: '/#', title: 'Copyright', copy: true },
+];
+Default.sites = [
+  { link: '/#', src: Logo1, alt: 'Biodiversity' },
+  { link: '/#', src: Logo2, alt: 'Climate and energy in the EU' },
+  { link: '/#', src: Logo3, alt: 'Copernicus in situ' },
+  {
+    link: '/#',
+    src: Logo4,
+    alt: 'Information platform for chemical monitoring',
   },
+  { link: '/#', src: Logo5, alt: 'WISE freshwater' },
+  { link: '/#', src: Logo6, alt: 'Forest information system for europe' },
+  { link: '/#', src: Logo7, alt: 'Climate adapt' },
+  { link: '/#', src: Logo8, alt: 'Copernicus land monitoring service' },
+  { link: '/#', src: Logo9, alt: 'European industrial emissions portal' },
+  { link: '/#', src: Logo10, alt: 'WISE marine' },
+];
+
+Default.social = [
+  { name: 'twitter', link: '/#' },
+  { name: 'facebook', link: '/#' },
+  { name: 'linkedin', link: '/#' },
+  { name: 'youtube', link: '/#' },
+  { name: 'rss', link: '/#' },
+];
+
+Default.contacts = {
+  header: 'Contact Us',
+  contacts: [
+    { icon: 'comment outline', text: 'Ask your question' },
+    { icon: 'envelope outline', text: 'Sign up to our newsletter' },
+  ],
+  address: 'Kongens Nytorv 6 1050 Copenhagen K (+45) 33 36 71 00',
 };
