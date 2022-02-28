0849f5545104412e62b1eb74921c5e86bcf951a2
Miu Razvan <miu.razvan28@gmail.com>
2022-02-28 17:54:55 +0200

    feat(blockquote): customize slate blockquote element to comply with eea design

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import BlockquoteElement from './ui/Blockquote/Blockquote';
+
 const applyConfig = (config) => {
+  // Apply blockquote slate customization
+  config.settings.slate.elements['blockquote'] = BlockquoteElement;
+
   return config;
 };
 
