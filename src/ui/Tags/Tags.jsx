e37fc1957ed8357bc2190239da4440bb98b0dabe
ITML <stamatis@itml.gr>
2022-03-01 15:00:22 +0000

    adds Tags

---
+++ src/ui/Tags/Tags.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Tags({ children, ...rest }) {
+  return (
+    <div {...rest} className={'eea tags ' + rest.className}>
+      {children}
+    </div>
+  );
+}
+
+Tags.Title = ({ children, ...rest }) => (
+  <span className="title" {...rest}>
+    {children}:
+  </span>
+);
+
+Tags.Tag = ({ children, ...rest }) => (
+  <a {...rest} className={'tag ' + rest.className}>
+    <span className="name">#{children}</span>
+  </a>
+);
+
+export default Tags;
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,12 @@
+const { default: Tags } = require("./Tags/Tags");
+
 export Banner from './Banner/Banner';
 
 export Blockquote from './Blockquote/Blockquote';
 
 export Breadcrumbs from './Breadcrumbs/Breadcrumbs';
+
+export Tags from './Tags/Tags';
 
 export Footer from './Footer/Footer';
 
