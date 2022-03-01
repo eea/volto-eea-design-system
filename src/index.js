90da95c67e965124da7f431e464bcbc15fa67894
Miu Razvan <miu.razvan28@gmail.com>
2022-03-01 17:38:57 +0200

    Typo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const applyConfig = (config) => {
   // Apply accordion block customization
   if (config.blocks.blocksConfig.accordion) {
     config.blocks.blocksConfig.accordion.semanticIcon = 'dropdown';
+  }
   // Apply blockquote slate customization
   if (config.settings.slate) {
     config.settings.slate.elements['blockquote'] = BlockquoteElement;
