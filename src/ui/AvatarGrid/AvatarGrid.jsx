b8e74f17df77f8f9f96bf442dc178417bbfc04c6
Odisseas Simou <odisseas.simou@itml.gr>
2022-03-02 13:14:11 +0000

    fix(storybook): lint auto changes for AvatarGrid

--- src/ui/AvatarGrid/AvatarGrid.jsx
+++ src/ui/AvatarGrid/AvatarGrid.jsx
@@ -3,38 +3,38 @@ import PropTypes from 'prop-types';
 import Avatar from '../Avatar/Avatar';
 
 AvatarGrid.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string,
 };
 
 function AvatarGrid({ children, ...rest }) {
-    return (
-        <div className="eea avatar-grid" {...rest}>
-            <div className="wrapper">{children}</div>
-        </div>
-    );
+  return (
+    <div className="eea avatar-grid" {...rest}>
+      <div className="wrapper">{children}</div>
+    </div>
+  );
 }
-
-
 
 AvatarGrid.Content = ({ children }) => {
-    return <div className="content">{children}</div>;
+  return <div className="content">{children}</div>;
 };
 
-AvatarGrid.Title = ({ children, ...rest }) =>  <p className={`grid-title  ${rest.showTitle ? '' :'hidden'}`}>{children}</p> ;
+AvatarGrid.Title = ({ children, ...rest }) => (
+  <p className={`grid-title  ${rest.showTitle ? '' : 'hidden'}`}>{children}</p>
+);
 AvatarGrid.Group = ({ children, ...rest }) => {
-    return (
-        <div className='avatar group'>
-            {rest.avatars.map((avatar, index) => (
-                <div className="avatar-wrapper">
-                    <Avatar {...rest} image_url={avatar.image_url} avatarSize="big">
-                        <Avatar.Content>
-                            <Avatar.Title>{avatar.title}</Avatar.Title>
-                            <Avatar.Metadata>{avatar.metadata}</Avatar.Metadata>
-                        </Avatar.Content>
-                    </Avatar>
-                </div>
-            ))}
+  return (
+    <div className="avatar group">
+      {rest.avatars.map((avatar, index) => (
+        <div className="avatar-wrapper">
+          <Avatar {...rest} image_url={avatar.image_url} avatarSize="big">
+            <Avatar.Content>
+              <Avatar.Title>{avatar.title}</Avatar.Title>
+              <Avatar.Metadata>{avatar.metadata}</Avatar.Metadata>
+            </Avatar.Content>
+          </Avatar>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 export default AvatarGrid;
--- src/ui/AvatarGrid/AvatarGrid.stories.jsx
+++ src/ui/AvatarGrid/AvatarGrid.stories.jsx
@@ -4,63 +4,63 @@ import imgUrl from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/i
 import AvatarGrid from './AvatarGrid';
 
 export default {
-    title: 'Components/Avatar Grid',
-    component: AvatarGrid,
-    argTypes: {
-        showTitle: {
-            description: 'show / hide title',
-            table: {
-                type: { summary: 'boolean' },
-                defaultValue: { summary: true },
-            },
-        },
-        title: {
-            description: 'avatar grid title',
-            table: {
-                defaultValue: { summary: '""' },
-                type: { summary: 'string' },
-            },
-        },
-        avatars: {
-            description: 'avatar data',
-            table: {
-                type: { summary: 'Object' },
-                defaultValue: { summary: ' "" ' },
-            },
-        },
-
+  title: 'Components/Avatar Grid',
+  component: AvatarGrid,
+  argTypes: {
+    showTitle: {
+      description: 'show / hide title',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: true },
+      },
     },
+    title: {
+      description: 'avatar grid title',
+      table: {
+        defaultValue: { summary: '""' },
+        type: { summary: 'string' },
+      },
+    },
+    avatars: {
+      description: 'avatar data',
+      table: {
+        type: { summary: 'Object' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+  },
 };
 
 const GridTemplate = (args) => (
-    <AvatarGrid>
-        <AvatarGrid.Content>
-            <AvatarGrid.Title showTitle={args.showTitle}>{args.title}</AvatarGrid.Title>
-            <AvatarGrid.Group avatars={args.avatars}>
-            </AvatarGrid.Group>
-        </AvatarGrid.Content>
-    </AvatarGrid>
+  <AvatarGrid>
+    <AvatarGrid.Content>
+      <AvatarGrid.Title showTitle={args.showTitle}>
+        {args.title}
+      </AvatarGrid.Title>
+      <AvatarGrid.Group avatars={args.avatars}></AvatarGrid.Group>
+    </AvatarGrid.Content>
+  </AvatarGrid>
 );
 
 export const Default = GridTemplate.bind({});
 Default.args = {
-    title: 'Amet - Lorem ipsum dolor sit amet',
-    showTitle: true,
-    avatars: [
-        {
-            image_url: imgUrl,
-            title: 'Lorem ipsum',
-            metadata: 'Urna auctor pharetra aenean sed gravida quam'
-        },
-        {
-            image_url: imgUrl,
-            title: 'Lorem ipsum',
-            metadata: 'Duis vel eu placerat proin diam id enim suspendisse erat'
-        },
-        {
-            image_url: imgUrl,
-            title: 'Lorem ipsum',
-            metadata: 'Egetisi gravida pellentesque volutpat diamon'
-        },
-    ],
+  title: 'Amet - Lorem ipsum dolor sit amet',
+  showTitle: true,
+  avatars: [
+    {
+      image_url: imgUrl,
+      title: 'Lorem ipsum',
+      metadata: 'Urna auctor pharetra aenean sed gravida quam',
+    },
+    {
+      image_url: imgUrl,
+      title: 'Lorem ipsum',
+      metadata: 'Duis vel eu placerat proin diam id enim suspendisse erat',
+    },
+    {
+      image_url: imgUrl,
+      title: 'Lorem ipsum',
+      metadata: 'Egetisi gravida pellentesque volutpat diamon',
+    },
+  ],
 };
