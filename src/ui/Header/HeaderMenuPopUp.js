28b04784cf32ed4393e452c56680bffda411d526
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-03-03 15:12:56 +0200

    Simplify overlay menu template

--- src/ui/Header/HeaderMenuPopUp.js
+++ src/ui/Header/HeaderMenuPopUp.js
@@ -1,7 +1,50 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import cx from 'classnames';
 
 import { useClickOutside } from '@eeacms/volto-eea-design-system/helpers';
+
+const levels = ['first', 'second', 'third'];
+
+const Item = ({ item }) => (
+  <>
+    {item.items.length > 0 && (
+      <label htmlFor={`drop-${item['@id'] || item.url}`} className="toggle">
+        {item.title}
+      </label>
+    )}
+    <a href={item['@id'] || item.url}>
+      <span>{item.title}</span>
+    </a>
+    {item.items.length > 0 && (
+      <input type="checkbox" id={`drop-${item['@id'] || item.url}`} />
+    )}
+  </>
+);
+
+const ItemsList = ({ items, level = 0 }) => (
+  <ul
+    className={cx(
+      level === 0 ? 'menu' : 'sub',
+      levels[level],
+      `level-${level}`,
+    )}
+  >
+    {items.map((item) => (
+      <li
+        key={item['@id'] || item.url}
+        className={cx({ hasSubMenu: item.items?.length > 0 })}
+      >
+        <Item item={item} />
+
+        {item.items.length > 0 && (
+          //second tier
+          <ItemsList items={item.items} level={level + 1} />
+        )}
+      </li>
+    ))}
+  </ul>
+);
 
 function HeaderMenuPopUp({ menuItems, onClose }) {
   const nodeRef = React.useRef();
@@ -12,80 +55,7 @@ function HeaderMenuPopUp({ menuItems, onClose }) {
     <div id="mega-menu" ref={nodeRef}>
       <Container>
         <nav>
-          <ul className="menu">
-            {menuItems.map((item) => (
-              //first tier
-              <li
-                key={item['@id'] || item.url}
-                className={item.items.length > 0 ? 'hasSubMenu' : null}
-              >
-                {item.items.length > 0 && (
-                  <label
-                    htmlFor={`drop-${item['@id'] || item.url}`}
-                    className="toggle"
-                  >
-                    {item.title}
-                  </label>
-                )}
-                <a href={item['@id'] || item.url}>
-                  <span>{item.title}</span>
-                </a>
-                {item.items.length > 0 && (
-                  <input
-                    type="checkbox"
-                    id={`drop-${item['@id'] || item.url}`}
-                  />
-                )}
-                {item.items.length > 0 && (
-                  //second tier
-                  <ul className="sub second">
-                    {item.items.map((section) => (
-                      <li
-                        key={section['@id'] || section.url}
-                        className={
-                          section.items.length > 0 ? 'hasSubMenu' : null
-                        }
-                      >
-                        {section.items.length > 0 && (
-                          <label
-                            htmlFor={`drop-${item['@id'] || item.url}-${
-                              section['@id'] || section.url
-                            }`}
-                            className="toggle"
-                          >
-                            <span>{section.title}</span>
-                          </label>
-                        )}
-                        <a href={section['@id'] || section.url}>
-                          <span>{section.title}</span>
-                        </a>
-                        {section.items.length > 0 && (
-                          <input
-                            type="checkbox"
-                            id={`drop-${item['@id'] || item.url}-${
-                              section['@id'] || section.url
-                            }`}
-                          ></input>
-                        )}
-                        {section.items.length > 0 && (
-                          //third tier
-                          <ul className="sub third">
-                            {section.items.map((page) => (
-                              <li key={page['@id'] || page.url}>
-                                <a href={page['@id'] || page.url}>
-                                  <span>{page.title}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
+          <ItemsList items={menuItems} />
         </nav>
       </Container>
     </div>
