946c6c6bc9dece0985ae4b9f9cad6ece2be0ff2c
nileshgulia1 <nileshgulia@gmail.com>
2022-03-05 20:52:02 +0530

    fix warning related to proptype

--- src/ui/Breadcrumbs/Breadcrumbs.jsx
+++ src/ui/Breadcrumbs/Breadcrumbs.jsx
@@ -10,25 +10,26 @@ import { Breadcrumb, Container, Image } from 'semantic-ui-react';
 
 import homeIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/home-icon.svg';
 
-const Breadcrumbs = ({ root, sections, icon }) => {
+const Breadcrumbs = ({ root, sections = [], icon }) => {
   return (
     <Container role="navigation" aria-label={'breadcrumbs'}>
       <Breadcrumb>
         <Link to={root || '/'} className="section" title={'Home'}>
           <Image src={homeIcon} alt="home" />
         </Link>
-        {sections.map((item, index, items) => [
-          <Breadcrumb.Divider icon={icon} key={`divider-${item.href}`} />,
-          index < items.length - 1 ? (
-            <Link key={item.key} to={item.href} className="section">
-              {item.title}
-            </Link>
-          ) : (
-            <Breadcrumb.Section key={item.key} active>
-              {item.title}
-            </Breadcrumb.Section>
-          ),
-        ])}
+        {sections.length > 0 &&
+          sections.map((item, index, items) => [
+            <Breadcrumb.Divider icon={icon} key={`divider-${item.href}`} />,
+            index < items.length - 1 ? (
+              <Link key={item.key} to={item.href} className="section">
+                {item.title}
+              </Link>
+            ) : (
+              <Breadcrumb.Section key={item.key} active>
+                {item.title}
+              </Breadcrumb.Section>
+            ),
+          ])}
       </Breadcrumb>
     </Container>
   );
