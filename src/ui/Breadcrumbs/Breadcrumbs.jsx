9f413ffaf641c161e8ff0f8010759cffd7e30238
nileshgulia1 <nileshgulia@gmail.com>
2022-03-07 22:53:37 +0530

    bring back segment wrapper and make it attached

--- src/ui/Breadcrumbs/Breadcrumbs.jsx
+++ src/ui/Breadcrumbs/Breadcrumbs.jsx
@@ -6,32 +6,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Breadcrumb, Container, Image } from 'semantic-ui-react';
+import { Breadcrumb, Container, Image, Segment } from 'semantic-ui-react';
 
 import homeIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/home-icon.svg';
 
 const Breadcrumbs = ({ root, sections = [], icon }) => {
   return (
-    <Container role="navigation" aria-label={'breadcrumbs'}>
-      <Breadcrumb>
-        <Link to={root || '/'} className="section" title={'Home'}>
-          <Image src={homeIcon} alt="home" />
-        </Link>
-        {sections.length > 0 &&
-          sections.map((item, index, items) => [
-            <Breadcrumb.Divider icon={icon} key={`divider-${item.href}`} />,
-            index < items.length - 1 ? (
-              <Link key={item.key} to={item.href} className="section">
-                {item.title}
-              </Link>
-            ) : (
-              <Breadcrumb.Section key={item.key} active>
-                {item.title}
-              </Breadcrumb.Section>
-            ),
-          ])}
-      </Breadcrumb>
-    </Container>
+    <Segment
+      role="navigation"
+      aria-label={'breadcrumbs'}
+      className="breadcrumbs"
+      attached
+    >
+      <Container>
+        <Breadcrumb>
+          <Link to={root || '/'} className="section" title={'Home'}>
+            <Image src={homeIcon} alt="home" />
+          </Link>
+          {sections.length > 0 &&
+            sections.map((item, index, items) => [
+              <Breadcrumb.Divider icon={icon} key={`divider-${item.href}`} />,
+              index < items.length - 1 ? (
+                <Link key={item.key} to={item.href} className="section">
+                  {item.title}
+                </Link>
+              ) : (
+                <Breadcrumb.Section key={item.key} active>
+                  {item.title}
+                </Breadcrumb.Section>
+              ),
+            ])}
+        </Breadcrumb>
+      </Container>
+    </Segment>
   );
 };
 
