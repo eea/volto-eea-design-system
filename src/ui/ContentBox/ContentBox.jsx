5aaf740594f0001b1672bb6dc18d973f55c881f0
Odisseas Simou <odisseas.simou@itml.gr>
2022-03-15 09:10:50 +0000

    chore(contentBox) : add lint changes

--- src/ui/ContentBox/ContentBox.jsx
+++ src/ui/ContentBox/ContentBox.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 
 function ContentBox({ children, ...rest }) {
-    return (
-        <div className='eea contentbox'>
-            <Grid>
-                <Grid.Column mobile={1} tablet={1} computer={2}></Grid.Column>
-                <Grid.Column mobile={10} tablet={10} computer={8}>
-                    <div className='title wrapper'>{rest.title}</div>
-                    <div className='body wrapper'>{rest.info}</div>
-                </Grid.Column>
-                <Grid.Column mobile={1} tablet={1} computer={2}></Grid.Column>
-            </Grid>
-        </div>
-    );
+  return (
+    <div className="eea contentbox">
+      <Grid>
+        <Grid.Column mobile={1} tablet={1} computer={2}></Grid.Column>
+        <Grid.Column mobile={10} tablet={10} computer={8}>
+          <div className="title wrapper">{rest.title}</div>
+          <div className="body wrapper">{rest.info}</div>
+        </Grid.Column>
+        <Grid.Column mobile={1} tablet={1} computer={2}></Grid.Column>
+      </Grid>
+    </div>
+  );
 }
 
 ContentBox.Content = ({ children }) => {
-    return <div className="content">{children}</div>;
+  return <div className="content">{children}</div>;
 };
 
 ContentBox.Title = ({ children }) => {
-    return <div className="title">{children}</div>;
+  return <div className="title">{children}</div>;
 };
 
 export default ContentBox;
