f0d2de67e43fafa10052871f99ac841c83cf8157
Antonis Tarantilis <antonis@itml.gr>
2022-03-24 11:49:40 +0000

    refactor(contentBox): restructure the component to be more flexible

--- src/ui/ContentBox/ContentBox.stories.js
+++ src/ui/ContentBox/ContentBox.stories.js
@@ -1,38 +1,57 @@
 import React from 'react';
 import ContentBox from './ContentBox';
+import { Container } from 'semantic-ui-react';
 // eslint-disable-next-line import/no-unresolved
 
 export default {
   title: 'Components/ContentBox',
   component: ContentBox,
   argTypes: {
-    title: {
-      description: 'content box title',
-      table: {
-        defaultValue: { summary: '""' },
-        type: { summary: 'string' },
-      },
-    },
-    info: {
-      description: 'content box secondary info',
-      table: {
-        defaultValue: { summary: '""' },
-        type: { summary: 'string' },
-      },
-    },
+    title: { description: 'Title' },
   },
 };
 
 const DefaultTemplate = (args) => (
-  <ContentBox {...args}>
-    <ContentBox.Title>{args.title}</ContentBox.Title>
-    <ContentBox.Content>{args.info}</ContentBox.Content>
-  </ContentBox>
+  <Container>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+      ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+      ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+      quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+      arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+      dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+      viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+    </p>
+    <ContentBox {...args}>
+      <h3>{args.title}</h3>
+      <p>{args.content}</p>
+    </ContentBox>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+      ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+      ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+      quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+      arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+      dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+      viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+    </p>
+  </Container>
 );
 
 export const Default = DefaultTemplate.bind({});
 Default.args = {
-  title: 'Lorem ipsum',
-  info:
-    '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"',
+  title: 'Lorem ipsum dolor sit amet',
+  content:
+    'Nullam orci magna congue facilisis fermentum tellus tincidunt. Elit, aliquet sit volutpat eu, nisi. Sed pretium mi et, ut. In consequat sit turpis tortor, pulvinar facilisis hendrerit risus vestibulum. Et at venenatis quis etiam purus. Quam mattis hendrerit enim nunc ullamcorper vestibulum, dictum purus aliquam. Felis ipsum habitasse non risus sapien facilisis augue auctor. Montes, vel aenean ultrices in in. Dolor a, id sapien, porttitor in quisque rutrum donec laoreet. Vestibulum sagittis, convallis posuere gravida odio. Ac et mattis scelerisque scelerisque neque scelerisque pellentesque nullam in. At nullam nec risus elementum.',
+  container: false,
 };
