6d8e3acb3bac1a4174d4bb526d6188cb5e5f8faa
Odisseas Simou <odisseas.simou@itml.gr>
2022-03-28 12:47:48 +0000

    style(downloadLabeledIcon) : fix styling

--- src/ui/DownloadLabeledIcon/DownloadLabeledIcon.stories.jsx
+++ src/ui/DownloadLabeledIcon/DownloadLabeledIcon.stories.jsx
@@ -20,7 +20,7 @@ export default {
                 type: { summary: 'string' },
             }
         },
-        items: {
+        links: {
             description: 'array of list content',
             table: {
                 type: { summary: 'object' },
@@ -31,21 +31,21 @@ export default {
 };
 
 const DefaultTemplate = (args) => (
-    <DownloadLabeledIcon {...args}>
+    <DownloadLabeledIcon {...args}>        
+        <DownloadLabeledIcon.Icon {...args}e>{args.icon}</DownloadLabeledIcon.Icon>
         <DownloadLabeledIcon.Label>{args.label}</DownloadLabeledIcon.Label>
-        <DownloadLabeledIcon.Icon>{args.icon}</DownloadLabeledIcon.Icon>
         <DownloadLabeledIcon.Dropdown {...args}></DownloadLabeledIcon.Dropdown>
     </DownloadLabeledIcon>
 );
 
 export const Default = DefaultTemplate.bind({});
 Default.args = {
-    title: 'Download',
-    icon: <i class="ri-download-line"></i>,
-
-    items: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa volutpat, feugiat lacus libero morbi dui. Ipsum vestibulum, faucibus aliquam pharetra ullamcorper accumsan. Mauris et blandit magna velit aliquam mattis tristique cursus. Gravida quis porta dictum sed nisl gravida. Dictum sapien purus ultrices leo felis quis.',
-        'Turpis feugiat felis id imperdiet imperdiet suspendisse. Sed pulvinar nisi, convallis sed non non eu, erat. ',
-        'Felis malesuada in elementum turpis. Enim, eu adipiscing sit fames ornare amet, suscipit. Neque, faucibus facilisi egestas vitae viverra tristique cum viverra elit. Nunc fermentum bibendum fames ultricies turpis habitant. ',
+    label: 'Download',
+    icon: "ri-download-line",
+    downloadIcon: 'ri-download-line',
+    links: [
+        { linkName: 'PDF', href: '#' },
+        { linkName: 'SVG', href: '#' },
+        { linkName: 'PNG', href: '#' },
     ],
 };
