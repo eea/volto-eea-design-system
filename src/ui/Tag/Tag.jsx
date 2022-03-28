ce9b6f92589618f6cb1e839a2cddc44381ef7406
Antonis Tarantilis <antonis@itml.gr>
2022-03-28 10:24:29 +0000

    refactor(tag): remove classes and actions that add background color

--- src/ui/Tag/Tag.jsx
+++ src/ui/Tag/Tag.jsx
@@ -3,7 +3,7 @@ import { Icon } from 'semantic-ui-react';
 
 function Tag({ children, ...rest }) {
   return (
-    <a {...rest} className={`tag ${rest.className || ''}`}>
+    <a {...rest} className="tag">
       <Icon className={`icon`} name="hashtag" />
       <span className="name">{children}</span>
     </a>
