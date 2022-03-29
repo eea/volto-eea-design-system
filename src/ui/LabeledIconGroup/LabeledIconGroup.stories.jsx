ab364763bb2526adbbb7fc3c656a16b497568940
Odisseas Simou <odisseas.simou@itml.gr>
2022-03-29 11:40:55 +0000

    chore(LabeledIconGroup) : lint changes

--- src/ui/LabeledIconGroup/LabeledIconGroup.stories.jsx
+++ src/ui/LabeledIconGroup/LabeledIconGroup.stories.jsx
@@ -3,52 +3,52 @@ import LabeledIconGroup from './LabeledIconGroup';
 // eslint-disable-next-line import/no-unresolved
 
 export default {
-    title: 'Components/Labeled Icons/Group',
-    component: LabeledIconGroup,
-    argTypes: {
-        newTab: {
-            description: 'NewTabLabeledIcon Component Contents',
-            table: {
-                type: { summary: 'Object' },
-                defaultValue: { summary: ' "" ' },
-            },
-        },
-        download: {
-            description: 'DownloadLabeledIcon Component Contents',
-            table: {
-                type: { summary: 'Object' },
-                defaultValue: { summary: ' "" ' },
-            },
-        }
+  title: 'Components/Labeled Icons/Group',
+  component: LabeledIconGroup,
+  argTypes: {
+    newTab: {
+      description: 'NewTabLabeledIcon Component Contents',
+      table: {
+        type: { summary: 'Object' },
+        defaultValue: { summary: ' "" ' },
+      },
     },
+    download: {
+      description: 'DownloadLabeledIcon Component Contents',
+      table: {
+        type: { summary: 'Object' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+  },
 };
 
 const DefaultTemplate = (args) => (
-    <div>
-        <LabeledIconGroup {...args}>
-            <LabeledIconGroup.Download {...args.download}></LabeledIconGroup.Download>
-            <LabeledIconGroup.NewTab {...args.newTab}>
-                {args.newTab.label}
-            </LabeledIconGroup.NewTab>
-        </LabeledIconGroup>
-    </div>
+  <div>
+    <LabeledIconGroup {...args}>
+      <LabeledIconGroup.Download {...args.download}></LabeledIconGroup.Download>
+      <LabeledIconGroup.NewTab {...args.newTab}>
+        {args.newTab.label}
+      </LabeledIconGroup.NewTab>
+    </LabeledIconGroup>
+  </div>
 );
 
 export const Default = DefaultTemplate.bind({});
 Default.args = {
-    newTab: {
-        label: 'Open in new Tab',
-        link: '/#',
-        icon: 'ri-share-box-fill',
-    },
-    download: {
-        label: 'Download',
-        icon: 'ri-download-2-fill',
-        downloadIcon: 'ri-download-2-fill',
-        links: [
-            { linkName: 'PDF', href: '/#' },
-            { linkName: 'SVG', href: '/#' },
-            { linkName: 'PNG', href: '/#' },
-        ],
-    },
+  newTab: {
+    label: 'Open in new Tab',
+    link: '/#',
+    icon: 'ri-share-box-fill',
+  },
+  download: {
+    label: 'Download',
+    icon: 'ri-download-2-fill',
+    downloadIcon: 'ri-download-2-fill',
+    links: [
+      { linkName: 'PDF', href: '/#' },
+      { linkName: 'SVG', href: '/#' },
+      { linkName: 'PNG', href: '/#' },
+    ],
+  },
 };
