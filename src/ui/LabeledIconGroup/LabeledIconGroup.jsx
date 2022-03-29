535cee4795dff75b35856cb4406a9bd33ee07ca4
Odisseas Simou <odisseas.simou@itml.gr>
2022-03-29 11:28:35 +0000

    chore(labeledIconGroup) : lint changes

--- src/ui/LabeledIconGroup/LabeledIconGroup.jsx
+++ src/ui/LabeledIconGroup/LabeledIconGroup.jsx
@@ -3,32 +3,36 @@ import NewTabLabeledIcon from '../NewTabLabeledIcon/NewTabLabeledIcon';
 import DownloadLabeledIcon from '../DownloadLabeledIcon/DownloadLabeledIcon';
 
 function LabeledIconGroup({ children, ...rest }) {
-    return (
-        <div className="eea labeled icon group" {...rest}>
-            {children}
-        </div>
-    );
+  return (
+    <div className="eea labeled icon group" {...rest}>
+      {children}
+    </div>
+  );
 }
 
 LabeledIconGroup.Download = ({ children, ...rest }) => {
-    return <div className='group wrapper'><DownloadLabeledIcon {...rest}>
+  return (
+    <div className="group wrapper">
+      <DownloadLabeledIcon {...rest}>
         <DownloadLabeledIcon.Icon {...rest}>
-            {rest.icon}
+          {rest.icon}
         </DownloadLabeledIcon.Icon>
         <DownloadLabeledIcon.Label>{rest.label}</DownloadLabeledIcon.Label>
         <DownloadLabeledIcon.Dropdown {...rest}></DownloadLabeledIcon.Dropdown>
-    </DownloadLabeledIcon></div>;
+      </DownloadLabeledIcon>
+    </div>
+  );
 };
 
 LabeledIconGroup.NewTab = ({ children, ...rest }) => {
-    return (
-        <div className='group wrapper'><NewTabLabeledIcon {...rest}>
-            <NewTabLabeledIcon.Icon {...rest}>
-                {rest.icon}
-            </NewTabLabeledIcon.Icon>
-            <NewTabLabeledIcon.Label>{rest.label}</NewTabLabeledIcon.Label>
-        </NewTabLabeledIcon></div>
-    );
+  return (
+    <div className="group wrapper">
+      <NewTabLabeledIcon {...rest}>
+        <NewTabLabeledIcon.Icon {...rest}>{rest.icon}</NewTabLabeledIcon.Icon>
+        <NewTabLabeledIcon.Label>{rest.label}</NewTabLabeledIcon.Label>
+      </NewTabLabeledIcon>
+    </div>
+  );
 };
 
 export default LabeledIconGroup;
--- src/ui/LabeledIconGroup/LabeledIconGroup.stories.jsx
+++ src/ui/LabeledIconGroup/LabeledIconGroup.stories.jsx
@@ -3,80 +3,80 @@ import LabeledIconGroup from './LabeledIconGroup';
 // eslint-disable-next-line import/no-unresolved
 
 export default {
-    title: 'Components/Labeled Icons/Group',
-    component: LabeledIconGroup,
-    argTypes: {
-        newTabLabel: {
-            description: 'New Tab Label',
-            table: {
-                defaultValue: { summary: '""' },
-                type: { summary: 'string' },
-            },
-        },
-        newTabLink: {
-            description: 'New Tab Link',
-            table: {
-                defaultValue: { summary: '""' },
-                type: { summary: 'string' },
-            },
-        },
-        newTabIcon: {
-            description: 'New Tab Icon',
-            table: {
-                defaultValue: { summary: '""' },
-                type: { summary: 'string' },
-            },
-        },
-        downloadLabel: {
-            description: 'Download Label',
-            table: {
-                defaultValue: { summary: '""' },
-                type: { summary: 'string' },
-            },
-        },
-        downloadIcon: {
-            description: 'Download Icon',
-            table: {
-                defaultValue: { summary: '""' },
-                type: { summary: 'string' },
-            },
-        },
-        downloadLinks: {
-            description: 'array of list content',
-            table: {
-                type: { summary: 'object' },
-                defaultValue: { summary: ' "" ' },
-            },
-        },
+  title: 'Components/Labeled Icons/Group',
+  component: LabeledIconGroup,
+  argTypes: {
+    newTabLabel: {
+      description: 'New Tab Label',
+      table: {
+        defaultValue: { summary: '""' },
+        type: { summary: 'string' },
+      },
     },
+    newTabLink: {
+      description: 'New Tab Link',
+      table: {
+        defaultValue: { summary: '""' },
+        type: { summary: 'string' },
+      },
+    },
+    newTabIcon: {
+      description: 'New Tab Icon',
+      table: {
+        defaultValue: { summary: '""' },
+        type: { summary: 'string' },
+      },
+    },
+    downloadLabel: {
+      description: 'Download Label',
+      table: {
+        defaultValue: { summary: '""' },
+        type: { summary: 'string' },
+      },
+    },
+    downloadIcon: {
+      description: 'Download Icon',
+      table: {
+        defaultValue: { summary: '""' },
+        type: { summary: 'string' },
+      },
+    },
+    downloadLinks: {
+      description: 'array of list content',
+      table: {
+        type: { summary: 'object' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+  },
 };
 
 const DefaultTemplate = (args) => (
-    <div>
-        <LabeledIconGroup {...args}>
-            <LabeledIconGroup.Download {...args.download}>
-            </LabeledIconGroup.Download>
-            <LabeledIconGroup.NewTab {...args.newTab}>{args.newTab.label}</LabeledIconGroup.NewTab>            
-        </LabeledIconGroup>
-    </div>
+  <div>
+    <LabeledIconGroup {...args}>
+      <LabeledIconGroup.Download {...args.download}></LabeledIconGroup.Download>
+      <LabeledIconGroup.NewTab {...args.newTab}>
+        {args.newTab.label}
+      </LabeledIconGroup.NewTab>
+    </LabeledIconGroup>
+  </div>
 );
 
 export const Default = DefaultTemplate.bind({});
 Default.args = {
-    newTab: {
-        label: 'Open in new Tab',
-        link: '/#',
-        icon: 'ri-share-box-fill',
-    },
-    download: {
-        label: 'Download',
-        icon: 'ri-download-2-fill',
-        downloadIcon: 'ri-download-2-fill',
-        links: [
-            { linkName: 'PDF', href: '#' },
-            { linkName: 'SVG', href: '#' },
-            { linkName: 'PNG', href: '#' },
-        ],
-    }
-
+  newTab: {
+    label: 'Open in new Tab',
+    link: '/#',
+    icon: 'ri-share-box-fill',
+  },
+  download: {
+    label: 'Download',
+    icon: 'ri-download-2-fill',
+    downloadIcon: 'ri-download-2-fill',
+    links: [
+      { linkName: 'PDF', href: '#' },
+      { linkName: 'SVG', href: '#' },
+      { linkName: 'PNG', href: '#' },
+    ],
+  },
 };
