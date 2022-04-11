850171e89d93eb207ca5b70a40f0fbfd2d9b4c97
ITML <cpanou@itml.gr>
2022-04-11 10:50:18 +0000

    feat(Toggle): create toggle story in forms

---
+++ src/ui/Form/Toggle.stories.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Checkbox, Container } from 'semantic-ui-react';
+
+export default {
+  title: 'Components/Forms/Toggle',
+  component: Toggle,
+  parameters: {
+    actions: {
+      handles: ['click'],
+    },
+  },
+  argTypes: {
+    label: {
+      description: 'Label of the toggle',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+    name: {
+      description: 'name for the toggle input',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+    id: {
+      description: 'id for the toggle input, also controls label for attribute',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+    disabled: {
+      description: 'controls the state of the toggle',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: true },
+      },
+    },
+  },
+};
+
+function Toggle({ ...args }) {
+  return (
+    <Container>
+      <Checkbox toggle {...args}></Checkbox>
+    </Container>
+  );
+}
+
+const Template = (args) => <Toggle {...args}></Toggle>;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: 'example',
+  label: 'Toggle label',
+  name: 'example',
+  disabled: false,
+};
