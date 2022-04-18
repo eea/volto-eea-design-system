37dc1f5c1506040c1f155afdea76c4ed739ddda2
David Ichim <ichim.david@gmail.com>
2022-04-18 09:40:15 +0300

    fixed js syntax error from previous commit

--- src/ui/Button/Button.stories.jsx
+++ src/ui/Button/Button.stories.jsx
@@ -119,7 +119,7 @@ export const Labeled = (args) => {
       </Button>
 
       <Button
-        className={args.variant + ' icon ' + args.icon_position ' labeled'}
+        className={args.variant + ' icon ' + args.icon_position + ' labeled'}
         disabled={args.disabled}
       >
         {args.icon_position === ' left' && (
