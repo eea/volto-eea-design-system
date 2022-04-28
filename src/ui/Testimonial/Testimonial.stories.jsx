7b6d128c6be3b2481d4765cebca82af6cafbea05
Alexis Sourtzis <a.sourtzis@itml.gr>
2022-04-28 12:00:03 +0000

    refactor(Testimonial):minor storybook bug fix

--- src/ui/Testimonial/Testimonial.stories.jsx
+++ src/ui/Testimonial/Testimonial.stories.jsx
@@ -15,15 +15,15 @@ export default {
         type: { summary: 'string' },
       },
     },
-    avatartitle: {
+    avatarTitle: {
       description: 'avatar title',
       type: { summary: 'string' },
     },
-    avatarinfo: {
+    avatarInfo: {
       description: 'avatar metadata',
       type: { summary: 'string' },
     },
-    avatarimage: {
+    avatarImage: {
       description: 'testimonial image',
       table: {
         defaultValue: { summary: '""' },
@@ -40,7 +40,7 @@ const InlineTemplate = (args) => (
   <Container>
     <Testimonial {...args}>
       <Testimonial.Avatar
-        src={imgUrl}
+        src={args.avatarImage}
         title={args.avatarTitle}
         description={args.avatarInfo}
       ></Testimonial.Avatar>
@@ -55,9 +55,9 @@ const InlineTemplate = (args) => (
 
 export const Inline = InlineTemplate.bind({});
 Inline.args = {
-  avatartitle: 'Jane Doe',
-  avatarinfo: 'EEA Analyst',
-  avatarimage: imgUrl,
+  avatarTitle: 'Jane Doe',
+  avatarInfo: 'EEA Analyst',
+  avatarImage: imgUrl,
   title: 'Amet - Lorem ipsum dolor sit amet',
   quote:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
