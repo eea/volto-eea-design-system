7f4ea476264856845a07cd24be6ec86ccd21ed8d
Antonis Tarantilis <antonis@itml.gr>
2022-05-19 14:55:43 +0000

    feat(ItemwithIcons): Initial Item group with icons, new item variation

---
+++ src/ui/Item/ItemGroupWithIcons.stories.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import { Item, Container, Grid } from 'semantic-ui-react';
+
+import GlobeEco from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Svg/globe-eco.svg';
+import Sustainable from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Svg/sustainable.svg';
+import DataAnalytics from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Svg/data-analytics.svg';
+import Network from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Svg/network.svg';
+import Knowledge from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Svg/knowledge.svg';
+
+export default {
+  title: 'Components/Item/Item group with icons',
+  component: Item,
+  argTypes: {
+    imageSize: {
+      control: { type: 'select' },
+      options: ['mini', 'default', 'tiny', 'small'],
+      description: 'Content can specify its vertical alignment',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+    verticalAlign: {
+      control: { type: 'select' },
+      options: ['top', 'middle', 'bottom'],
+      description: 'An image can vary in size',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: ' "" ' },
+      },
+    },
+  },
+};
+
+function SingleItem({ image, imageSize, description, verticalAlign }) {
+  return (
+    <Item>
+      {/* <Item.Image size={imageSize == 'default' ? '' : imageSize} src={image} alt="item image" /> */}
+      <Item.Image size={imageSize} src={image} alt="item image" />
+      <Item.Content verticalAlign={verticalAlign}>{description}</Item.Content>
+    </Item>
+  );
+}
+
+// Single item
+export const DefaultItem = (args) => (
+  <Container>
+    <Item.Group relaxed>
+      <SingleItem {...args}></SingleItem>
+    </Item.Group>
+  </Container>
+);
+DefaultItem.args = {
+  image: GlobeEco,
+  imageSize: 'default',
+  verticalAlign: 'middle',
+  description:
+    'Support Europe’s transition to a sustainable future with evidence-based knowledge and data.',
+};
+DefaultItem.argTypes = {
+  image: {
+    description: 'Path or Url of the image',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: ' "" ' },
+    },
+  },
+  description: {
+    description: 'Item content description',
+    table: {
+      type: { summary: 'string' },
+      defaultValue: { summary: ' "" ' },
+    },
+  },
+};
+
+function ItemGroup({ imageSize, verticalAlign, ColumnLeft, ColumnRight }) {
+  return (
+    <Container>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column mobile={12} tablet={6} computer={6}>
+            <Item.Group relaxed unstackable>
+              {ColumnLeft.map((item) => (
+                <SingleItem key={item.childKey} {...item}></SingleItem>
+              ))}
+            </Item.Group>
+          </Grid.Column>
+          <Grid.Column mobile={12} tablet={6} computer={6}>
+            <Item.Group relaxed unstackable>
+              {ColumnRight.map((item) => (
+                <SingleItem key={item.childKey} {...item}></SingleItem>
+              ))}
+            </Item.Group>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
+}
+
+const Template = (args) => <ItemGroup {...args}></ItemGroup>;
+
+// Group of items
+export const DefaultGroup = Template.bind({});
+DefaultGroup.args = {
+  ColumnLeft: [
+    {
+      childKey: 0,
+      image: GlobeEco,
+      imageSize: 'default',
+      verticalAlign: 'middle',
+      description:
+        'Support Europe’s transition to a sustainable future with evidence-based knowledge and data.',
+    },
+    {
+      childKey: 1,
+      image: Sustainable,
+      imageSize: 'default',
+      verticalAlign: 'middle',
+      description:
+        'Supply input on solutions to the sustainability challenges of today – and tomorrow.  ',
+    },
+    {
+      childKey: 2,
+      image: DataAnalytics,
+      imageSize: 'default',
+      verticalAlign: 'middle',
+      description:
+        'Leverage the data and technology to support the EU’s decision-making processes concerning the environment and climate',
+    },
+  ],
+  ColumnRight: [
+    {
+      childKey: 0,
+      image: Network,
+      imageSize: 'default',
+      verticalAlign: 'middle',
+      description:
+        'Build stronger networks and partnerships with state and regional governments and organisations.',
+    },
+    {
+      childKey: 1,
+      image: Knowledge,
+      imageSize: 'default',
+      verticalAlign: 'middle',
+      description:
+        'Strengthen the EU’s knowledge capacity and help secure the resources needed to create a sustainable Europe.',
+    },
+  ],
+};
