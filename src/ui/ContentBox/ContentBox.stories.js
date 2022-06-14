87cb2fe7e4cd5c2872299756f493e371abcbb05f
Alin Voinea <contact@avoinea.com>
2022-06-14 08:19:14 +0300

    feat(ContentBox): Add primary, secondary, tertiary themes

--- src/ui/ContentBox/ContentBox.stories.js
+++ src/ui/ContentBox/ContentBox.stories.js
@@ -53,5 +53,5 @@ Default.args = {
   title: 'Lorem ipsum dolor sit amet',
   content:
     'Nullam orci magna congue facilisis fermentum tellus tincidunt. Elit, aliquet sit volutpat eu, nisi. Sed pretium mi et, ut. In consequat sit turpis tortor, pulvinar facilisis hendrerit risus vestibulum. Et at venenatis quis etiam purus. Quam mattis hendrerit enim nunc ullamcorper vestibulum, dictum purus aliquam. Felis ipsum habitasse non risus sapien facilisis augue auctor. Montes, vel aenean ultrices in in. Dolor a, id sapien, porttitor in quisque rutrum donec laoreet. Vestibulum sagittis, convallis posuere gravida odio. Ac et mattis scelerisque scelerisque neque scelerisque pellentesque nullam in. At nullam nec risus elementum.',
-  container: false,
+  container: true,
 };
