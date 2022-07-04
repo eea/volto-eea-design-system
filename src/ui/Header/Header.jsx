96671574f5f7dd992269c42e509598f62bba516d
Antonis Tarantilis <antonis@itml.gr>
2022-07-04 13:06:12 +0000

    refactor(megaMenu): add slide down transition and update targetRefs on close

--- src/ui/Header/Header.jsx
+++ src/ui/Header/Header.jsx
@@ -120,6 +120,7 @@ const Main = ({
   };
 
   const mobileBurgerOnClick = () => {
+    console.log('aa', burger);
     if (searchIsActive === true) {
       setSearchIsActive(false);
     }
@@ -164,6 +165,7 @@ const Main = ({
   const node = React.useRef();
   const searchButtonRef = React.useRef();
   const mobileMenuBurgerRef = React.useRef();
+  const desktopMenuRef = React.useRef();
 
   // disable sticky setting until feature is more stable
   // const isScrollingUp = useScrollingUp();
@@ -185,7 +187,10 @@ const Main = ({
           <Grid.Column mobile={4} tablet={4} computer={8}>
             <div className={inverted ? 'main-menu inverted' : 'main-menu'}>
               {menuItems && (
-                <Menu className="eea-main-menu tablet or lower hidden" text>
+                <div
+                  className="ui text eea-main-menu tablet or lower hidden menu"
+                  ref={desktopMenuRef}
+                >
                   {menuItems.map((item) => (
                     <Menu.Item
                       name={item['@id'] || item.url}
@@ -200,7 +205,7 @@ const Main = ({
                       })}
                     </Menu.Item>
                   ))}
-                </Menu>
+                </div>
               )}
               <div
                 className="search-action"
@@ -234,15 +239,14 @@ const Main = ({
           triggerRefs={[searchButtonRef]}
         />
       )}
-      {menuIsActive && (
-        <HeaderMenuPopUp
-          renderMenuItem={renderMenuItem}
-          activeItem={activeItem}
-          menuItems={menuItems}
-          onClose={menuOnClickOutside}
-          triggerRefs={[mobileMenuBurgerRef]}
-        />
-      )}
+      <HeaderMenuPopUp
+        renderMenuItem={renderMenuItem}
+        activeItem={activeItem}
+        menuItems={menuItems}
+        onClose={menuOnClickOutside}
+        triggerRefs={[mobileMenuBurgerRef, desktopMenuRef]}
+        visible={menuIsActive}
+      />
     </div>
   );
 };
