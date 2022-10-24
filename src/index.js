d937df7c262e8d8cc6e78eebec736e28263fc657
David Ichim <ichim.david@gmail.com>
2022-10-24 21:17:05 +0300

    feature(content): added NarrowView useful for modifying the containers from content-area

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+import NarrowView from '@eeacms/volto-eea-design-system/components/theme/Views/NarrowView';
+
 const applyConfig = (config) => {
   config.settings = {
     ...(config.settings || {}),
     navDepth: 3,
+  };
+
+  // Custom Views layouts
+  config.views.layoutViews = {
+    ...(config.views.layoutViews || {}),
+    narrow_view: NarrowView,
+  };
+  config.views.layoutViewsNamesMapping = {
+    ...(config.views.layoutViewsNamesMapping || {}),
+    narrow_view: 'Narrow view',
   };
   return config;
 };
