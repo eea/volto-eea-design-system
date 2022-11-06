fad86ae581dd81d0c29fc03d8528e57cadadd3fb
David Ichim <ichim.david@gmail.com>
2022-11-06 15:06:48 +0200

    removed console log from code

--- src/components/theme/Views/LayoutAppExtrasView.jsx
+++ src/components/theme/Views/LayoutAppExtrasView.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BodyClass } from '@plone/volto/helpers';
 
 const LayoutAppExtrasView = (props) => {
-  console.log(props);
   return <BodyClass className={props.content?.layout} />;
 };
 
