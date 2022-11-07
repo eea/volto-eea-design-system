f6dc1ee602634df85140f6db75e41293b72eacf1
David Ichim <ichim.david@gmail.com>
2022-11-07 18:54:11 +0200

    change(container): removed app extras and view setting of layout sizes
    - we moved to a block approach added in volto-eea-website-policy which
      can be added within the layout control panel to set defaults for any
      given content type
    - give a greater specificity to the sizing classes added on the content-area
      this way they override the body classes and they are more resilient to less
      specific css overrides

--- src/components/theme/Views/LayoutAppExtrasView.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { BodyClass } from '@plone/volto/helpers';
-
-const LayoutAppExtrasView = (props) => {
-  return <BodyClass className={props.content?.layout} />;
-};
-
-export default LayoutAppExtrasView;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
-import NarrowView from '@eeacms/volto-eea-design-system/components/theme/Views/NarrowView';
-import LayoutAppExtrasView from '@eeacms/volto-eea-design-system/components/theme/Views/LayoutAppExtrasView';
-
 const applyConfig = (config) => {
   config.settings = {
     ...(config.settings || {}),
     navDepth: 3,
   };
-
-  // Custom Views layouts
-  config.views.layoutViews = {
-    ...(config.views.layoutViews || {}),
-    narrow_view: NarrowView,
-  };
-  config.views.layoutViewsNamesMapping = {
-    ...(config.views.layoutViewsNamesMapping || {}),
-    narrow_view: 'Narrow view',
-  };
-
-  const appExtras = config.settings.appExtras || [];
-  config.settings.appExtras = [
-    ...appExtras,
-    {
-      match: '',
-      component: LayoutAppExtrasView,
-    },
-  ];
 
   return config;
 };
