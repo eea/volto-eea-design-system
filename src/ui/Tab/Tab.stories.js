9a18fe22f24d73a1b5ab9174f0a1aca3ff80febf
Antonis Tarantilis <antonis@itml.gr>
2023-04-18 14:29:06 +0000

    refactor(tabs): accessibility updates

--- src/ui/Tab/Tab.stories.js
+++ src/ui/Tab/Tab.stories.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Tab, Container } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Tab, Container, Menu, Grid } from 'semantic-ui-react';
 
 export default {
   title: 'Components/Tab',
@@ -11,7 +11,6 @@ export default {
   },
   argTypes: {
     panes: {
-      description: 'tab names and content array',
       table: {
         type: {
           summary: 'object',
@@ -25,8 +24,10 @@ export default {
 };
 
 function TabContent({
+  args,
   panes,
   renderActive,
+  id,
   text,
   attached,
   borderless,
@@ -34,10 +35,57 @@ function TabContent({
   fluid,
   grid,
 }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeTab, setActiveTab] = useState(panes[0]);
+
+  const panez = panes.map((tab, index) => {
+    return {
+      menuItem: (
+        <Menu.Item
+          key={`Tab ${index}`}
+          name={`Tab ${index}`}
+          active={tab === activeTab}
+          tabIndex={0}
+          onClick={() => {
+            if (activeTab !== tab) {
+              setActiveIndex(index);
+              setActiveTab(tab);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              if (
+                document
+                  .getElementById(id)
+                  ?.getElementsByClassName('active tab').length > 0
+              ) {
+                const tabDiv = document
+                  .getElementById(id)
+                  .getElementsByClassName('active tab')[0];
+                tabDiv.focus();
+              }
+              if (activeTab !== tab) {
+                setActiveIndex(index);
+                setActiveTab(tab);
+              }
+            }
+          }}
+        >
+          <p className={'menu-item-text'}>{tab.menuItem}</p>
+        </Menu.Item>
+      ),
+      render: () => {
+        return <>{tab.render()}</>;
+      },
+    };
+  });
+
   return (
     <Tab
-      panes={panes}
+      panes={panez}
       renderActiveOnly={renderActive}
+      className="default tabs tabs-accessibility"
       menu={{
         secondary: true,
         pointing: true,
@@ -46,6 +94,7 @@ function TabContent({
         tabIndex: 0,
       }}
       grid={grid}
+      activeIndex={activeIndex}
     ></Tab>
   );
 }
@@ -58,23 +107,88 @@ const Template = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  renderActive: false,
+  renderActive: true,
+  id: 'tab',
   panes: [
     {
-      menuItem: 'Tab 1',
-      pane: 'Tab 1 Content',
+      menuItem: 'Ground level ozone',
+      render: () => (
+        <Tab.Pane>
+          <Grid>
+            <Grid.Column mobile={12} tablet={9} computer={9}>
+              is not directly emitted into the atmosphere. Instead, it forms in
+              the atmosphere from a chain of chemical reactions following
+              emissions of certain precursor gases: NOX, carbon monoxide (CO)
+              and NMVOCs and methane (CH4). Ground-level ozone is formed from
+              chemical reactions in the presence of sunlight, following
+              emissions of precursor gases, mainly NOX, NMVOCs, CO and CH4.
+            </Grid.Column>
+            <Grid.Column mobile={12} tablet={3} computer={3}>
+              <img
+                src="https://www.eea.europa.eu/publications/managing-the-systemic-use-of/image_preview"
+                alt="sources of air pollution"
+                className="ui fluid image"
+              />
+            </Grid.Column>
+          </Grid>
+        </Tab.Pane>
+      ),
     },
     {
-      menuItem: 'Tab 2',
-      pane: 'Tab 2 Content',
+      menuItem: 'Nitrogen oxides',
+      render: () => (
+        <Tab.Pane>
+          {' '}
+          are emitted during fuel combustion from industrial facilities and the
+          road transport sector. NOX is a group of gases comprising nitrogen
+          monoxide (NO) and nitrogen dioxide (NO2). NO makes up the majority of
+          NOX emissions. NOX contributes to the formation of ozone and
+          particulate matter.
+        </Tab.Pane>
+      ),
     },
     {
-      menuItem: 'Tab 3',
-      pane: 'Tab 3 Content',
+      menuItem: 'Particulate matter',
+      render: () => (
+        <Tab.Pane>
+          {' '}
+          is a mixture of aerosol particles (solid and liquid) covering a wide
+          range of sizes and chemical compositions. PM is either directly
+          emitted as primary particles or it forms in the atmosphere from
+          emissions of certain precursor pollutants such as SO2, NOX, NH3 and
+          NMVOCs. PM is emitted from many anthropogenic sources, including both
+          combustion and non-combustion sources. Natural emissions of PM also
+          occur, including from sea salt and windblown Saharan dust.
+        </Tab.Pane>
+      ),
     },
     {
-      menuItem: 'Tab 4',
-      pane: 'Tab 4 Content',
+      menuItem: 'Sulphur dioxide',
+      render: () => (
+        <Tab.Pane>
+          {' '}
+          is formed and emitted by combustion of fossil fuels (mainly coal and
+          oil) primarily for electricity generation. High concentrations of SO2
+          are associated with multiple health and environmental effects. The
+          highest concentrations of SO2 have been recorded in the vicinity of
+          large industrial facilities. SO2 emissions are an important
+          environmental issue because they are a major precursor to ambient
+          PM2.5 concentrations.
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: 'Benzopyrene',
+      render: () => (
+        <Tab.Pane>
+          {' '}
+          is a polycyclic aromatic hydrocarbon (PAH) found in fine PM. Its
+          origin is the incomplete combustion of various fuels. The main sources
+          of BaP in Europe are domestic home-heating, in particular wood- and
+          coal-burning, waste-burning, coke and steel production, and road
+          traffic. Other sources include outdoor fires.
+        </Tab.Pane>
+      ),
     },
   ],
 };
@@ -90,18 +204,23 @@ Vertical.args = {
       menuItem: 'Ground level ozone',
       render: () => (
         <Tab.Pane>
-          {' '}
-          is not directly emitted into the atmosphere. Instead, it forms in the
-          atmosphere from a chain of chemical reactions following emissions of
-          certain precursor gases: NOX, carbon monoxide (CO) and NMVOCs and
-          methane (CH4).{' '}
-          <img
-            src="https://www.eea.europa.eu/signals/signals-2013/infographics/sources-of-air-pollution-in-europe"
-            alt="sources of air pollution"
-          />{' '}
-          Ground-level ozone is formed from chemical reactions in the presence
-          of sunlight, following emissions of precursor gases, mainly NOX,
-          NMVOCs, CO and CH4.
+          <Grid>
+            <Grid.Column mobile={12} tablet={8} computer={8}>
+              is not directly emitted into the atmosphere. Instead, it forms in
+              the atmosphere from a chain of chemical reactions following
+              emissions of certain precursor gases: NOX, carbon monoxide (CO)
+              and NMVOCs and methane (CH4). Ground-level ozone is formed from
+              chemical reactions in the presence of sunlight, following
+              emissions of precursor gases, mainly NOX, NMVOCs, CO and CH4.
+            </Grid.Column>
+            <Grid.Column mobile={12} tablet={4} computer={4}>
+              <img
+                src="https://www.eea.europa.eu/en/topics/in-depth/air-pollution/31905041720_2c248b6224_cropped.jpg/@@images/image-529-0b40801694a97fa192d849d632f99632.jpeg"
+                alt="sources of air pollution"
+                className="ui fluid image"
+              />
+            </Grid.Column>
+          </Grid>
         </Tab.Pane>
       ),
     },
@@ -166,7 +285,6 @@ Vertical.args = {
 
 Vertical.argTypes = {
   vertical: {
-    description: 'vertical tab',
     table: {
       type: {
         summary: 'boolean',
@@ -177,7 +295,6 @@ Vertical.argTypes = {
     },
   },
   fluid: {
-    description: 'fluid tabs',
     table: {
       type: {
         summary: 'boolean',
@@ -188,8 +305,6 @@ Vertical.argTypes = {
     },
   },
   grid: {
-    description:
-      'Shorthand props for the Grid. Only applicable to vertical menus',
     table: {
       type: {
         summary: 'object',
