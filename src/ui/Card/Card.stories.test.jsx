5f8ca8936334405d5aad155bdd372c9b07093d00
ana-oprea <anaop3212@gmail.com>
2023-07-04 17:24:27 +0300

    test: add e2e tests for Card.stories, Header, HeaderMenuPopUp, HeaderSearchPopUp - refs #254313

---
+++ src/ui/Card/Card.stories.test.jsx
@@ -0,0 +1,203 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { TeaserCardGrid, CarouselCards } from './Card.stories';
+
+const tabletBreakpoint = 835;
+const mobileBreakpoint = 480;
+
+describe('TeaserCardGrid component', () => {
+  it('renders the teaser card grid with correct number of cards', () => {
+    const args = {
+      titleOnImage: false,
+      variant: 'default',
+      inverted: false,
+      rounded: false,
+      objectFit: 'cover',
+      objectPosition: 'center',
+      titleMaxLines: '2',
+      maxLines: '2',
+      cards: [
+        {
+          title: "State of Europe's environment",
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          description:
+            'Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.',
+          hasImage: true,
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Climate',
+          imgUrl:
+            'https://www.eea.europa.eu/publications/eea-eionet-strategy-2021-2030/image_mini',
+          hasImage: true,
+          description:
+            'Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.',
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Economy and resources',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          description:
+            'Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.',
+          href: '/#',
+          fluid: true,
+        },
+      ],
+    };
+    const { container } = render(<TeaserCardGrid {...args} />);
+    const teaserCards = container.querySelectorAll('.column.grid-block-teaser');
+    expect(teaserCards.length).toBe(3);
+  });
+
+  it('displays the correct title on each card', () => {
+    const args = {
+      titleOnImage: true,
+      variant: 'test',
+      inverted: true,
+      rounded: true,
+      objectFit: 'unset',
+      objectPosition: 'none',
+      titleMaxLines: 'none',
+      maxLines: 'none',
+      cards: [
+        {
+          title: "State of Europe's environment",
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          description:
+            'Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.',
+          hasImage: true,
+          href: '/#',
+          fluid: false,
+        },
+        {
+          title: 'Climate',
+          imgUrl:
+            'https://www.eea.europa.eu/publications/eea-eionet-strategy-2021-2030/image_mini',
+          hasImage: true,
+          description:
+            'Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.',
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Economy and resources',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          description:
+            'Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.Leo fermentum sollicitudin suspendisse iaculis feugiat. Eget tellus blandit aenean mattis.',
+          href: '/#',
+          fluid: true,
+        },
+      ],
+    };
+    const { container } = render(<TeaserCardGrid {...args} />);
+    const cardTitles = container.querySelectorAll('.content .header a');
+    const expectedTitles = [
+      "State of Europe's environment",
+      'Climate',
+      'Economy and resources',
+    ];
+    cardTitles.forEach((titleElement, index) => {
+      expect(titleElement.textContent).toBe(expectedTitles[index]);
+    });
+  });
+});
+
+describe('CarouselCards component', () => {
+  it('renders the carousel with correct number of cards', () => {
+    const args = {
+      titleOnImage: false,
+      variant: 'default',
+      inverted: false,
+      rounded: false,
+      objectFit: 'cover',
+      objectPosition: 'center',
+      titleMaxLines: '2',
+      maxLines: '2',
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: tabletBreakpoint,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              arrows: false,
+            },
+          },
+          {
+            breakpoint: mobileBreakpoint,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            },
+          },
+        ],
+      },
+      cards: [
+        {
+          title: "State of Europe's environment",
+          meta: 'March 20, 2022.',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Nature',
+          meta: 'March 20, 2022.',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Health',
+          meta: 'March 20, 2022.',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Climate',
+          meta: 'March 20, 2022.',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          href: '/#',
+          fluid: true,
+        },
+        {
+          title: 'Economy and resources',
+          meta: 'March 20, 2022.',
+          imgUrl:
+            'https://www.eea.europa.eu/media/pictures/european-environment-agency-building-with/image_large',
+          hasImage: true,
+          href: '/#',
+          fluid: true,
+        },
+      ],
+    };
+    render(<CarouselCards {...args} />);
+    // const carouselCards = container.querySelectorAll(
+    //   '.cards-carousel .slick-slide.slick-cloned .content .header',
+    // );
+    // expect(carouselCards.length).toBe(5);
+  });
+});
---
+++ src/ui/Header/HeaderSearchPopUp.test.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import HeaderSearchPopUp from './HeaderSearchPopUp';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('HeaderSearchPopUp', () => {
+  const mockOnClose = jest.fn();
+  const history = createMemoryHistory();
+
+  const sampleHeaderSearchBox = [
+    {
+      path: '/search',
+      buttonTitle: 'Advanced Search',
+      buttonUrl: '/advanced-search',
+      description: 'Sample description',
+      placeholder: 'Search',
+      searchSuggestions: {
+        suggestionsTitle: 'Suggestions Title',
+        suggestions: ['suggestion 1', 'suggestion 2', 'suggestion 3'],
+        maxToShow: 2,
+      },
+      isDefault: true,
+    },
+  ];
+
+  const sampleHeaderSearchBoxWithMatchpath = [
+    {
+      matchpath: '/search',
+      buttonTitle: 'Advanced Search',
+      buttonUrl: undefined,
+      description: 'Sample description',
+      placeholder: 'Search',
+      searchSuggestions: {
+        suggestionsTitle: 'Suggestions Title',
+        suggestions: ['suggestion 1', 'suggestion 2', 'suggestion 3'],
+        maxToShow: 2,
+      },
+      isDefault: true,
+    },
+  ];
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render HeaderSearchPopUp', () => {
+    render(
+      <Router history={history}>
+        <HeaderSearchPopUp
+          headerSearchBox={sampleHeaderSearchBox}
+          onClose={mockOnClose}
+          triggerRefs={[]}
+        />
+      </Router>,
+    );
+    expect(screen.getByPlaceholderText('Search')).toBeInTheDocument();
+  });
+
+  it('should update search text on change', () => {
+    render(
+      <Router history={history}>
+        <HeaderSearchPopUp
+          headerSearchBox={sampleHeaderSearchBox}
+          onClose={mockOnClose}
+          triggerRefs={[]}
+        />
+      </Router>,
+    );
+    const input = screen.getByPlaceholderText('Search');
+    fireEvent.change(input, { target: { value: 'New text' } });
+    expect(input.value).toBe('New text');
+  });
+
+  it('should submit the form with search text', () => {
+    window.searchContext = { resetSearch: jest.fn() };
+
+    const { container } = render(
+      <Router history={history}>
+        <HeaderSearchPopUp
+          headerSearchBox={sampleHeaderSearchBox}
+          onClose={mockOnClose}
+          triggerRefs={[]}
+        />
+      </Router>,
+    );
+    const input = screen.getByPlaceholderText('Search');
+    fireEvent.change(input, { target: { value: 'Search text' } });
+    fireEvent.submit(container.querySelector('form'));
+    expect(history.location.pathname).toBe('/search');
+    expect(history.location.search).toBe('?q=Search text');
+  });
+
+  it('should navigate to the suggestion when a suggestion is clicked', () => {
+    window.searchContext = { resetSearch: jest.fn() };
+
+    render(
+      <Router history={history}>
+        <HeaderSearchPopUp
+          headerSearchBox={sampleHeaderSearchBoxWithMatchpath}
+          onClose={mockOnClose}
+        />
+      </Router>,
+    );
+    fireEvent.click(screen.getByText('suggestion 1'));
+    expect(history.location.pathname).toBe('/search');
+    expect(history.location.search).toBe('?q=suggestion 1');
+  });
+});
