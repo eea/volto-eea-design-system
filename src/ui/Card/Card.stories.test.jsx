c9fb9634e97bd28cabcda13582613ecf1b60c597
ana-oprea <anaop3212@gmail.com>
2023-07-05 10:54:05 +0300

    chore: ESlint code cleanup - refs #254313

--- src/ui/Card/Card.stories.test.jsx
+++ src/ui/Card/Card.stories.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { TeaserCardGrid, CarouselCards } from './Card.stories';
 
 const tabletBreakpoint = 835;
