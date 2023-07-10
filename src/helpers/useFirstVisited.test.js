c9458b8f28f62f2c6a80d35bec319ed820cb8719
ana-oprea <anaop3212@gmail.com>
2023-07-10 16:59:13 +0300

    chore: fix ESlint - refs #254313

--- src/helpers/useFirstVisited.test.js
+++ src/helpers/useFirstVisited.test.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import useFirstVisited from './useFirstVisited';
 
 describe('useFirstVisited', () => {
