0f61c8c2fd2baee93f8e1efca36bc058d8871d55
ana-oprea <anaop3212@gmail.com>
2023-07-10 13:37:54 +0300

    test: add unit tests for Accordion - refs #254313

---
+++ src/ui/Accordion/Accordion.stories.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Default } from './Accordion.stories';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Default component', () => {
+  it('renders cirrectly and fires keyDown Enter events', () => {
+    const { getByText, getAllByText } = render(<Default {...Default.args} />);
+
+    Default.args.panels.forEach((panel) => {
+      expect(getByText(panel.title)).toBeInTheDocument();
+    });
+
+    expect(getAllByText(Default.args.panels[0].content)).toHaveLength(3);
+    fireEvent.keyDown(getByText(Default.args.panels[0].title), {
+      key: 'Enter',
+      code: 'Enter',
+      keyCode: 13,
+      charCode: 13,
+    });
+
+    fireEvent.keyDown(getByText(Default.args.panels[0].title), {
+      key: 'Enter',
+      code: 'Enter',
+      keyCode: 13,
+      charCode: 13,
+    });
+  });
+
+  it('renders cirrectly and fires keyDown A event', () => {
+    const { getByText, getAllByText } = render(
+      <Default {...Default.args} title_size={'h1'} />,
+    );
+
+    Default.args.panels.forEach((panel) => {
+      expect(getByText(panel.title)).toBeInTheDocument();
+    });
+
+    expect(getAllByText(Default.args.panels[0].content)).toHaveLength(3);
+    fireEvent.keyDown(getByText(Default.args.panels[0].title), {
+      key: 'A',
+      code: 'KeyA',
+    });
+  });
+});
