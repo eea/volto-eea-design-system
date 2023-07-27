aeff67bc3a38239a26c18c4611c7d7498c75ba1d
Cretu Mihaela <cretu.mihaelaa11@gmail.com>
2023-07-27 15:37:24 +0300

    remove settings

--- src/index.js
+++ src/index.js
@@ -2,26 +2,6 @@ const applyConfig = (config) => {
   config.settings = {
     ...(config.settings || {}),
     navDepth: 3,
-    megaMenuLayouts: {
-      '/en/topics': {
-        columns: 4,
-        columnsWidth: [4, 8],
-        itemsEquallySpread: true,
-        hideChildrenFromNavigation: false,
-      },
-      '/en/countries': {
-        columns: 4,
-        columnsWidth: [8, 4],
-        itemsEquallySpread: false,
-        hideChildrenFromNavigation: false,
-      },
-      '/en/about': {
-        columns: 1,
-        columnsWidth: [3, 3, 3, 3],
-        itemsEquallySpread: true,
-        hideChildrenFromNavigation: false,
-      },
-    },
   };
 
   return config;
