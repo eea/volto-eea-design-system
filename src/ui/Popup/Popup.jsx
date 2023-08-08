3fe26294162966d3815d74384f5e53071ad80b26
David Ichim <ichim.david@gmail.com>
2023-08-08 22:11:21 +0300

    change(popup): use a functional component for popup component using react-popper

--- src/ui/Popup/Popup.jsx
+++ src/ui/Popup/Popup.jsx
@@ -1,8 +1,5 @@
-/* Simplified popup with several options found in the semantic ui implementation
- * https://github.com/Semantic-Org/Semantic-UI-React/blob/master/src/modules/Popup/Popup.js
- *  */
-import React from 'react';
-import { createPopper } from '@popperjs/core';
+import React, { useEffect, useRef, useState } from 'react';
+import { usePopper } from 'react-popper';
 import EventStack from '@semantic-ui-react/event-stack';
 import cx from 'classnames';
 
@@ -10,139 +7,106 @@ export const positionsMapping = {
   'top center': 'top',
   'top left': 'top-start',
   'top right': 'top-end',
-
   'bottom center': 'bottom',
   'bottom left': 'bottom-start',
   'bottom right': 'bottom-end',
-
   'right center': 'right',
   'left center': 'left',
 };
 
-class Popup extends React.Component {
-  constructor(props) {
-    super(props);
+function Popup(props) {
+  const triggerRef = useRef(null);
+  const popupRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.triggerRef = React.createRef();
-    this.popupRef = React.createRef();
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-
-    this.state = {
-      isOpen: false,
-    };
-
-    this.togglePopup = this.togglePopup.bind(this);
-    this.closeOnEscape = this.closeOnEscape.bind(this);
-  }
-
-  componentDidMount() {
-    this.popper = createPopper(this.triggerRef.current, this.popupRef.current, {
-      placement: positionsMapping[this.props.position] || 'bottom-end',
-      strategy: this.props.positionFixed || 'absolute',
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: this.props.offset,
-          },
+  const options = {
+    placement: positionsMapping[props.position] || 'bottom-end',
+    strategy: props.positionFixed || 'absolute',
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: props.offset,
         },
-        ...this.props.popperModifiers,
-      ],
-    });
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
+      },
+      ...props.popperModifiers,
+    ],
+  };
 
-  componentWillUnmount() {
-    this.popper && this.popper.destroy();
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  }
+  const { styles, attributes } = usePopper(
+    triggerRef.current,
+    popupRef.current,
+    options,
+  );
 
-  UNSAFE_componentWillUpdate(nextProps, nextState, nextContext) {
-    const position = nextProps.position;
+  const handleClickOutside = (event) => {
     if (
-      position &&
-      this.popper.state.placement !== positionsMapping[position]
+      popupRef.current &&
+      !popupRef.current.contains(event.target) &&
+      !triggerRef.current.contains(event.target)
     ) {
-      this.popper.setOptions({
-        placement: positionsMapping[position] || 'bottom-end',
-      });
-    }
-  }
-
-  handleClickOutside(event) {
-    if (
-      this.popupRef &&
-      !this.popupRef.current.contains(event.target) &&
-      !this.triggerRef.current.contains(event.target)
-    ) {
-      if (this.state.isOpen) {
-        this.setState((state) => {
-          return {
-            isOpen: false,
-          };
-        });
+      if (isOpen) {
+        setIsOpen(false);
       }
     }
-  }
+  };
 
-  togglePopup() {
-    this.setState(
-      (state) => {
-        return {
-          isOpen: !state.isOpen,
-        };
-      },
-      () => {
-        this.popper.forceUpdate();
-      },
-    );
-  }
-  closeOnEscape(e) {
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  const togglePopup = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
+
+  const closeOnEscape = (e) => {
     if (e.key === 'Escape') {
-      this.setState((state) => {
-        return {
-          isOpen: !state.isOpen,
-        };
-      });
+      setIsOpen((prevIsOpen) => !prevIsOpen);
     }
-  }
+  };
 
-  render() {
-    const { trigger, className, size, position, basic, content } = this.props;
-    const event = this.props.on;
-    const onEvent = 'on' + event.charAt(0).toUpperCase() + event.slice(1);
-    return (
-      <React.Fragment>
-        {trigger && (
-          <div className="popup-trigger" ref={this.triggerRef}>
-            {React.cloneElement(trigger, {
-              [onEvent]: this.togglePopup,
-            })}
-          </div>
-        )}
+  const { trigger, className, size, position, basic, content, on } = props;
 
-        <div className="popup-container" ref={this.popupRef}>
-          {this.state.isOpen && (
-            <EventStack name="keydown" on={this.closeOnEscape} />
-          )}
-          <React.Fragment>
-            <div
-              className={cx(
-                'ui popup transition',
-                className,
-                size,
-                position,
-                basic ? 'basic' : '',
-                this.state.isOpen ? 'visible' : '',
-              )}
-            >
-              {content}
-            </div>
-          </React.Fragment>
+  const onEvent = 'on' + on.charAt(0).toUpperCase() + on.slice(1);
+
+  return (
+    <React.Fragment>
+      {trigger && (
+        <div className="popup-trigger" ref={triggerRef}>
+          {React.cloneElement(trigger, {
+            [onEvent]: togglePopup,
+          })}
         </div>
-      </React.Fragment>
-    );
-  }
+      )}
+
+      <div
+        className="popup-container"
+        ref={popupRef}
+        style={styles.popper}
+        {...attributes.popper}
+      >
+        {isOpen && <EventStack name="keydown" on={closeOnEscape} />}
+        <React.Fragment>
+          <div
+            className={cx(
+              'ui popup transition',
+              className,
+              size,
+              position,
+              basic ? 'basic' : '',
+              isOpen ? 'visible' : '',
+            )}
+          >
+            {content}
+          </div>
+        </React.Fragment>
+      </div>
+    </React.Fragment>
+  );
 }
 
 Popup.defaultProps = {
@@ -155,14 +119,6 @@ Popup.defaultProps = {
   wide: false,
   on: 'click',
   popperModifiers: [],
-  // disabled,
-  // flowing,
-  // header,
-  // inverted,
-  // pinned,
-  // popper,
-  // popperDependencies,
-  // style,
 };
 
 export default Popup;
