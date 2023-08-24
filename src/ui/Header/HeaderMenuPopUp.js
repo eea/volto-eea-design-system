f30ee908c1a3ae83050765426be87beaf51b3a62
David Ichim <ichim.david@gmail.com>
2023-08-24 20:44:16 +0300

    removed aria-expanded duplication of merge from devel and simplified setActiveIndex

--- src/ui/Header/HeaderMenuPopUp.js
+++ src/ui/Header/HeaderMenuPopUp.js
@@ -287,13 +287,8 @@ const NestedAccordion = ({ menuItems, renderMenuItem, pathName }) => {
         aria-expanded={activeIndex === index}
         as="button"
         onClick={() => {
-          if (activeIndex === index) {
-            setActiveIndex(-1);
-          } else {
-            setActiveIndex(index);
-          }
+          setActiveIndex(activeIndex === index ? -1 : index);
         }}
-        aria-expanded={activeIndex === index}
       >
         {element.title}
         <Icon className="ri-arrow-down-s-line" size="small" />
