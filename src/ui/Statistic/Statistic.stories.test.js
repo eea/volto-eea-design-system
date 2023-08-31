4ec6099685f93c58a858282d3791643931d7cb64
David Ichim <ichim.david@gmail.com>
2023-08-31 18:59:08 +0300

    lint fix

--- src/ui/Statistic/Statistic.stories.test.js
+++ src/ui/Statistic/Statistic.stories.test.js
@@ -12,17 +12,15 @@ import '@testing-library/jest-dom/extend-expect';
 describe('AnimationGroup component', () => {
   let observe;
   let unobserve;
-  let callback;
   let disconnect;
   observe = jest.fn();
   unobserve = jest.fn();
   disconnect = jest.fn();
 
-  window.IntersectionObserver = jest.fn(function (cb) {
+  window.IntersectionObserver = jest.fn(function () {
     this.observe = observe;
     this.unobserve = unobserve;
     this.disconnect = disconnect;
-    callback = cb;
   });
   it('renders a button with label', () => {
     const { getByText } = render(<AnimationGroup {...AnimationGroup.args} />);
@@ -42,17 +40,15 @@ describe('AnimationGroup component', () => {
 describe('Animation component', () => {
   let observe;
   let unobserve;
-  let callback;
   let disconnect;
   observe = jest.fn();
   unobserve = jest.fn();
   disconnect = jest.fn();
 
-  window.IntersectionObserver = jest.fn(function (cb) {
+  window.IntersectionObserver = jest.fn(function () {
     this.observe = observe;
     this.unobserve = unobserve;
     this.disconnect = disconnect;
-    callback = cb;
   });
   it('renders a button with label', () => {
     const { getByText } = render(<Animation {...Animation.args} />);
