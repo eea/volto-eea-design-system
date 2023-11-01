b9cc7ad14495a0ccb443de9c4650d9b5b7bea0bc
Antonis Tarantilis <antonis@itml.gr>
2023-11-01 08:41:02 +0000

    refactor(Tabs): update accessibility and vertical position
    -add keyboard accessibility
    -add role and aria-selected to tab items
    -add control for right tabs position on vertical
    -fix inverted for vertical
    -restracture panes to avoid duplications

--- src/ui/Tab/Tab.stories.js
+++ src/ui/Tab/Tab.stories.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Tab, Container } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Tab, Container, Menu } from 'semantic-ui-react';
 
 export default {
   title: 'Components/Tab',
@@ -20,8 +20,17 @@ export default {
         type: { summary: 'string' },
       },
     },
+    inverted: {
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
     panes: {
-      description: 'tab names and content array',
       table: {
         type: {
           summary: 'object',
@@ -34,6 +43,79 @@ export default {
   },
 };
 
+const panes = [
+  {
+    menuItem: 'Ground level ozone',
+    render: () => (
+      <Tab.Pane>
+        {' '}
+        is not directly emitted into the atmosphere. Instead, it forms in the
+        atmosphere from a chain of chemical reactions following emissions of
+        certain precursor gases: NOX, carbon monoxide (CO) and NMVOCs and
+        methane (CH4). Ground-level ozone is formed from chemical reactions in
+        the presence of sunlight, following emissions of precursor gases, mainly
+        NOX, NMVOCs, CO and CH4.
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Nitrogen oxides',
+    render: () => (
+      <Tab.Pane>
+        {' '}
+        are emitted during fuel combustion from industrial facilities and the
+        road transport sector. NOX is a group of gases comprising nitrogen
+        monoxide (NO) and nitrogen dioxide (NO2). NO makes up the majority of
+        NOX emissions. NOX contributes to the formation of ozone and particulate
+        matter.
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Particulate matter',
+    render: () => (
+      <Tab.Pane>
+        {' '}
+        is a mixture of aerosol particles (solid and liquid) covering a wide
+        range of sizes and chemical compositions. PM is either directly emitted
+        as primary particles or it forms in the atmosphere from emissions of
+        certain precursor pollutants such as SO2, NOX, NH3 and NMVOCs. PM is
+        emitted from many anthropogenic sources, including both combustion and
+        non-combustion sources. Natural emissions of PM also occur, including
+        from sea salt and windblown Saharan dust.
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Sulphur dioxide',
+    render: () => (
+      <Tab.Pane>
+        {' '}
+        is formed and emitted by combustion of fossil fuels (mainly coal and
+        oil) primarily for electricity generation. High concentrations of SO2
+        are associated with multiple health and environmental effects. The
+        highest concentrations of SO2 have been recorded in the vicinity of
+        large industrial facilities. SO2 emissions are an important
+        environmental issue because they are a major precursor to ambient PM2.5
+        concentrations.
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Benzopyrene',
+    render: () => (
+      <Tab.Pane>
+        {' '}
+        is a polycyclic aromatic hydrocarbon (PAH) found in fine PM. Its origin
+        is the incomplete combustion of various fuels. The main sources of BaP
+        in Europe are domestic home-heating, in particular wood- and
+        coal-burning, waste-burning, coke and steel production, and road
+        traffic. Other sources include outdoor fires.
+      </Tab.Pane>
+    ),
+  },
+];
+
 function TabContent({
   panes,
   variant,
@@ -43,20 +125,68 @@ function TabContent({
   pointing,
   fluid,
   grid,
+  position,
 }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeTab, setActiveTab] = useState(panes[0]);
+
+  const accessiblePanes = panes.map((tab, index) => {
+    return {
+      menuItem: (
+        <Menu.Item
+          key={`Tab ${index}`}
+          name={`Tab ${index}`}
+          active={tab === activeTab}
+          tabIndex={0}
+          onClick={() => {
+            if (activeTab !== tab) {
+              setActiveIndex(index);
+              setActiveTab(tab);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              if (document.getElementsByClassName('active tab').length > 0) {
+                const tabDiv = document.getElementsByClassName('active tab')[0];
+                tabDiv.focus();
+              }
+              if (activeTab !== tab) {
+                setActiveIndex(index);
+                setActiveTab(tab);
+              }
+            }
+          }}
+          role="tab"
+          aria-selected={tab === activeTab}
+        >
+          <p className={'menu-item-text'}>{tab.menuItem}</p>
+        </Menu.Item>
+      ),
+      render: () => {
+        return <>{tab.render()}</>;
+      },
+    };
+  });
+
   return (
     <Tab
-      panes={panes}
+      panes={accessiblePanes}
       renderActiveOnly={renderActive}
+      menuPosition={position}
+      className="default tabs tabs-accessibility"
       menu={{
         vertical: vertical,
         inverted: inverted,
         pointing: pointing,
         fluid: fluid,
-        className: variant,
+        className: `${
+          position === 'right' ? 'border-left ' + variant : variant
+        }`,
         tabIndex: 0,
       }}
       grid={grid}
+      activeIndex={activeIndex}
     ></Tab>
   );
 }
@@ -69,26 +199,10 @@ const Template = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  renderActive: false,
+  variant: 'secondary',
+  renderActive: true,
   inverted: false,
-  panes: [
-    {
-      menuItem: 'Tab 1',
-      pane: 'Tab 1 Content',
-    },
-    {
-      menuItem: 'Tab 2',
-      pane: 'Tab 2 Content',
-    },
-    {
-      menuItem: 'Tab 3',
-      pane: 'Tab 3 Content',
-    },
-    {
-      menuItem: 'Tab 4',
-      pane: 'Tab 4 Content',
-    },
-  ],
+  panes: [...panes],
 };
 
 export const Vertical = Template.bind({});
@@ -97,92 +211,15 @@ Vertical.args = {
   renderActive: true,
   vertical: true,
   pointing: false,
+  inverted: false,
   fluid: true,
-  grid: { paneWidth: 7, tabWidth: 5 },
-  panes: [
-    {
-      menuItem: 'Ground level ozone',
-      render: () => (
-        <Tab.Pane>
-          {' '}
-          is not directly emitted into the atmosphere. Instead, it forms in the
-          atmosphere from a chain of chemical reactions following emissions of
-          certain precursor gases: NOX, carbon monoxide (CO) and NMVOCs and
-          methane (CH4).{' '}
-          <img
-            src="https://www.eea.europa.eu/signals/signals-2013/infographics/sources-of-air-pollution-in-europe"
-            alt="sources of air pollution"
-          />{' '}
-          Ground-level ozone is formed from chemical reactions in the presence
-          of sunlight, following emissions of precursor gases, mainly NOX,
-          NMVOCs, CO and CH4.
-        </Tab.Pane>
-      ),
-    },
-    {
-      menuItem: 'Nitrogen oxides',
-      render: () => (
-        <Tab.Pane>
-          {' '}
-          are emitted during fuel combustion from industrial facilities and the
-          road transport sector. NOX is a group of gases comprising nitrogen
-          monoxide (NO) and nitrogen dioxide (NO2). NO makes up the majority of
-          NOX emissions. NOX contributes to the formation of ozone and
-          particulate matter.
-        </Tab.Pane>
-      ),
-    },
-    {
-      menuItem: 'Particulate matter',
-      render: () => (
-        <Tab.Pane>
-          {' '}
-          is a mixture of aerosol particles (solid and liquid) covering a wide
-          range of sizes and chemical compositions. PM is either directly
-          emitted as primary particles or it forms in the atmosphere from
-          emissions of certain precursor pollutants such as SO2, NOX, NH3 and
-          NMVOCs. PM is emitted from many anthropogenic sources, including both
-          combustion and non-combustion sources. Natural emissions of PM also
-          occur, including from sea salt and windblown Saharan dust.
-        </Tab.Pane>
-      ),
-    },
-    {
-      menuItem: 'Sulphur dioxide',
-      render: () => (
-        <Tab.Pane>
-          {' '}
-          is formed and emitted by combustion of fossil fuels (mainly coal and
-          oil) primarily for electricity generation. High concentrations of SO2
-          are associated with multiple health and environmental effects. The
-          highest concentrations of SO2 have been recorded in the vicinity of
-          large industrial facilities. SO2 emissions are an important
-          environmental issue because they are a major precursor to ambient
-          PM2.5 concentrations.
-        </Tab.Pane>
-      ),
-    },
-    {
-      menuItem: 'Benzopyrene',
-      render: () => (
-        <Tab.Pane>
-          {' '}
-          is a polycyclic aromatic hydrocarbon (PAH) found in fine PM. Its
-          origin is the incomplete combustion of various fuels. The main sources
-          of BaP in Europe are domestic home-heating, in particular wood- and
-          coal-burning, waste-burning, coke and steel production, and road
-          traffic. Other sources include outdoor fires.
-        </Tab.Pane>
-      ),
-    },
-  ],
+  position: 'left',
+  grid: { paneWidth: 8, tabWidth: 4 },
+  panes: [...panes],
 };
 
 Vertical.argTypes = {
-  variant: 'secondary',
-  inverted: false,
   vertical: {
-    description: 'vertical tab',
     table: {
       type: {
         summary: 'boolean',
@@ -192,8 +229,17 @@ Vertical.argTypes = {
       },
     },
   },
+  position: {
+    name: 'menu position',
+    defaultValue: 'left',
+    options: ['left', 'right'],
+    control: { type: 'select' },
+    table: {
+      defaultValue: { summary: 'left' },
+      type: { summary: 'string' },
+    },
+  },
   fluid: {
-    description: 'fluid tabs',
     table: {
       type: {
         summary: 'boolean',
@@ -204,8 +250,6 @@ Vertical.argTypes = {
     },
   },
   grid: {
-    description:
-      'Shorthand props for the Grid. Only applicable to vertical menus',
     table: {
       type: {
         summary: 'object',
