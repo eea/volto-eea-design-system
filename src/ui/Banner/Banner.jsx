dd4548de999787eff688b4ee36860d69459735c3
Alin Voinea <contact@avoinea.com>
2023-11-21 12:24:03 +0200

    fix: Regression on Button.Action component when used with RSS link

--- src/ui/Banner/Banner.jsx
+++ src/ui/Banner/Banner.jsx
@@ -78,7 +78,14 @@ Banner.Action = React.forwardRef(function (
 ) {
   return (
     <div className="action" ref={ref}>
-      <Button className={className} basic icon inverted onClick={onClick}>
+      <Button
+        className={className}
+        basic
+        icon
+        inverted
+        onClick={onClick}
+        {...rest}
+      >
         <Icon className={icon} color={color} title={title}></Icon>
         <span className={titleClass || 'mobile-sr-only'}>{title}</span>
       </Button>
