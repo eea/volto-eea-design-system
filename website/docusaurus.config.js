98eb380cc8cb98db62cd795c915aa69c60b115fa
Teodor <teodorvoicu87@gmail.com>
2024-02-20 14:02:59 +0200

    fix: remove noreferrer for seo purposes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -27,7 +27,7 @@ module.exports = {
     announcementBar: {
       id: 'alpha-release',
       content:
-        '🚧 The EEA Design System is currently <strong>Work-In-Progress</strong>. See <a target="_blank" rel="noopener noreferrer" href="/volto-eea-design-system/docs/whatsnew">What\'s new</a> for more information. 🚧',
+        '🚧 The EEA Design System is currently <strong>Work-In-Progress</strong>. See <a target="_blank" rel="noopener" href="/volto-eea-design-system/docs/whatsnew">What\'s new</a> for more information. 🚧',
       backgroundColor: '#fafbfc',
       textColor: '#091E42',
       isCloseable: false,
