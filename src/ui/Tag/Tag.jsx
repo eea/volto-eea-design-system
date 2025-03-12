704e3a41174acdc4f01d1c38aef4e59e0e0164a0
Teodor <teodorvoicu87@gmail.com>
2025-03-12 14:42:42 +0200

    Update Tag component to render ConditionalLink component

--- src/ui/Tag/Tag.jsx
+++ src/ui/Tag/Tag.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
+import { ConditionalLink } from '@plone/volto/components';
+import { getFieldURL } from '@eeacms/volto-tags-block/helpers';
 
-function Tag({ children, ...rest }) {
+function Tag({ href, children }) {
   return (
-    <a {...rest} className="tag">
-      <Icon className={`icon`} name="hashtag" />
-      <span className="name">{children}</span>
-    </a>
+    <div className={`tag ${href ? 'link' : ''}`}>
+      <ConditionalLink
+        condition={href}
+        to={href ? getFieldURL(href) : null}
+        openLinkInNewTab={false}
+      >
+        <Icon className={`icon`} name="hashtag" />
+        <span className="name">{children}</span>
+      </ConditionalLink>
+    </div>
   );
 }
 
 export default Tag;
+
+{
+  /* <div className={`tag ${href ? 'link' : ''}`}>
+<ConditionalLink
+  condition={href}
+  to={href ? getFieldURL(href) : null}
+  openLinkInNewTab={openLinkInNewTab}
+>
+  {showTagIcon && <Icon className={`icon`} name="hashtag" />}
+  <span className="name">{children}</span>
+</ConditionalLink>
+</div> */
+}
+
+{
+  /* <a {...rest} className="tag link">
+<Icon className={`icon`} name="hashtag" />
+<span className="name">{children}</span>
+</a> */
+}
